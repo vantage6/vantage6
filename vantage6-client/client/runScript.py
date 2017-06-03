9873c24b2026e59b871dfefd19958296ab2a84d8
Johan van Soest <johan.vansoest@maastro.nl>
2017-06-03 20:09:27 +0200

    Added time interval and config file

--- vantage6-client/client/runScript.py
+++ vantage6-client/client/runScript.py
@@ -1,27 +1,33 @@
-import json, requests
+import json, requests, time
 
 #connect to service
 headerData = {
     'Content-Type': "application/json"
 }
 
-clientData = {
-    'name': "Johan van Soest",
-    'email': "johan.vansoest@maastro.nl",
-    'institute': 'MAASTRO Clinic',
-    'country': 'Netherlands'
-}
-# execute HTTP POST to try and authenticate
-resp = requests.post("http://localhost:5000/client/add", data=json.dumps(clientData), headers=headerData)
-respObj = json.loads(resp.text)
-clientId = respObj.get('clientId', '')
+configFile = open("config.json")
+clientData = json.load(configFile)
+configFile.close()
+
+if "id" not in clientData:
+    # execute HTTP POST to try and authenticate
+    resp = requests.post("http://localhost:5000/client/add", data=json.dumps(clientData), headers=headerData)
+    respObj = json.loads(resp.text)
+    clientId = respObj.get('clientId', '')
+    clientData["id"] = clientId
+    configFile = open("config.json", "w")
+    json.dump(clientData, configFile)
+    configFile.close()
+
+print("Starting with client ID " + str(clientData["id"]))
 
 while True:
-    resp = requests.get("http://localhost:5000/client/"+str(clientId)+"/task")
+    resp = requests.get("http://localhost:5000/client/"+str(clientData["id"])+"/task")
     respObjTask = json.loads(resp.text)
 
     if len(respObjTask) == 0:
         print("nothing to do....")
+        time.sleep(clientData["interval"])
     else :
         
         taskId = respObjTask.get('id')
@@ -34,7 +40,7 @@ while True:
                 'response': "Hello, world!"
             }
             # execute HTTP POST to try and authenticate
-            resp = requests.post("http://localhost:5000/client/" + str(clientId) + "/task/" + str(taskId) + "/add", data=json.dumps(clientData), headers=headerData)
+            resp = requests.post("http://localhost:5000/client/" + str(clientData["id"]) + "/task/" + str(taskId) + "/add", data=json.dumps(clientData), headers=headerData)
             respObjResult = json.loads(resp.text)
             resultId = respObjResult.get('resultId', '')
             print("resultId" + str(resultId))
