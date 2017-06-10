ee65a1bdaabef8e8e6461268b55283e3d66c3099
Johan van Soest <johan.vansoest@maastro.nl>
2017-06-10 16:57:22 +0200

    Added docker support

--- vantage6/client/runScript.py
+++ vantage6/client/runScript.py
@@ -1,15 +1,20 @@
 import json
 import requests
 import time
+import docker
 
 # connect to service
 headerData = {
     'Content-Type': "application/json"
 }
 
+# Init configuration file
 configFile = open("config.json")
 clientData = json.load(configFile)
 configFile.close()
+
+# Init docker
+dockerClient = docker.from_env()
 
 if "id" not in clientData:
     # execute HTTP POST to try and authenticate
@@ -24,13 +29,21 @@ if "id" not in clientData:
 print("Starting with client ID " + str(clientData["id"]))
 
 while True:
-    resp = requests.get("http://localhost:5000/client/" + str(clientData["id"]) + "/task")
-    taskList = json.loads(resp.text)
+    taskList = list()
 
+    # Connect to central host, if fails do nothing
+    try:
+        resp = requests.get("http://localhost:5000/client/" + str(clientData["id"]) + "/task")
+        taskList = json.loads(resp.text)
+    except:
+        print("Could not retrieve result from master.")
+
+    # If no task retrieved (or central host down) wait again
     if len(taskList) == 0:
-        print("nothing to do....")
+        print("Waiting....")
         time.sleep(clientData["interval"])
 
+    # If there are tasks retrieved, proces task list
     iTask = 0
     while iTask < len(taskList):
         myTask = taskList[iTask]
@@ -39,13 +52,12 @@ while True:
         inputArgs = myTask.get("input")
         imageResponse = ""
 
-        if image == "hello-world":
-            print("found hello-world!")
-            imageResponse = "Hello, world!"
+        logResult = dockerClient.containers.run(image)
 
         responseData = {
-            'response': imageResponse
+            'response': logResult
         }
+
         # execute HTTP POST to send back result (response)
         resp = requests.post(
             "http://localhost:5000/client/" + str(clientData["id"]) + "/task/" + str(taskId) + "/result/add",
