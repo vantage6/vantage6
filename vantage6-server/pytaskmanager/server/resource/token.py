76f43b5071e63958c3fd4e970bfcd5d979ad16da
Frank Martin <f.martin@iknl.nl>
2018-06-07 16:31:13 +0200

    minor bugfix: node_url is set correctly in the case a node authenticates

--- vantage6-server/pytaskmanager/server/resource/token.py
+++ vantage6-server/pytaskmanager/server/resource/token.py
@@ -77,7 +77,7 @@ class Token(Resource):
                 ret = {
                     'access_token': create_access_token(node),
                     'refresh_token': create_refresh_token(node),
-                    'node_url': server.api.url_for(server.resource.node.Node, uid=node.id),
+                    'node_url': server.api.url_for(server.resource.node.Node, id=node.id),
                     'refresh_url': server.api.url_for(RefreshToken),
                 }
                 log.info("Authenticated as node '{}' ({})".format(node.id, node.name))
