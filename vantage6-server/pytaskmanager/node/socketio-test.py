2c7cb33676ebace86e997e28aa391870ed1453c5
Melle Sieswerda <melle@sieswerda.net>
2018-11-05 11:39:39 +0100

    Proof of concept for socketio.
    Uses:
    * https://flask-socketio.readthedocs.io/en/latest/
    * https://github.com/invisibleroads/socketIO-client

---
+++ vantage6-server/pytaskmanager/node/socketio-test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+from socketIO_client import SocketIO, SocketIONamespace, LoggingNamespace
+
+# import logging
+# logging.getLogger().setLevel(logging.DEBUG)
+# logging.getLogger('socketIO-client').setLevel(logging.DEBUG)
+# logging.basicConfig()
+
+class Namespace(SocketIONamespace):
+    def on_connect(self):
+        print('on_connect')
+
+    def on_disconnect(self):
+        print('on_disconnect')
+
+    def on_reconnect(self):
+        print('on_reconnect')
+
+    def on_my_event(self, *args):
+        print('on_my_event', args)
+
+    def on_message(self, *args):
+        print('on_message', args)
+        
+    def on_event(self, *args):
+        print('on_event', args)
+    
+
+# Setup socketIO connection to localhost:5000
+token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NDEzNjYxNzgsIm5iZiI6MTU0MTM2NjE3OCwianRpIjoiYjA5ODY0NzQtNTNiMy00YzBjLTlkYjEtN2Y4MmU5ODgwZTc1IiwiZXhwIjoxNTQxNDUyNTc4LCJpZGVudGl0eSI6MSwiZnJlc2giOmZhbHNlLCJ0eXBlIjoiYWNjZXNzIiwidXNlcl9jbGFpbXMiOnsidHlwZSI6InVzZXIiLCJyb2xlcyI6WyJhZG1pbiJdfX0.GvLaxVXpvpOHtpIsbFubezI2WwMIWZSLSixvX0tqyig"
+headers = {
+    "Authorization": f"Bearer {token}",
+}
+socketIO = SocketIO('127.0.0.1', 5000, Namespace, headers=headers)
+print(f'I am connected: {socketIO.connected}')
+
+# Listen / connect event handlers
+# socketIO.on('connect', on_connect)
+# socketIO.on('disconnect', on_disconnect)
+# socketIO.on('reconnect', on_reconnect)
+# socketIO.on('my event', on_my_event)
+# socketIO.on('message', on_message)
+
+# socketIO.emit('my event')
+# socketIO.send('this sends a regular message (it just creates an event with type "message")')
+
+socketIO.emit('join')
+
+socketIO.wait(seconds=5)
+
