89f56ebdac86d0f5cc5790ef4400a88532565150
Melle Sieswerda <melle@sieswerda.net>
2018-12-03 14:35:54 +0100

    websocket implementation for xterm.js. Connect to the shell at http://localhost:5000/static/index.html

--- vantage6/pytaskmanager/server/__init__.py
+++ vantage6/pytaskmanager/server/__init__.py
@@ -3,6 +3,14 @@
 import os, sys
 import importlib
 
+# Stuff needed for running shell in a browser
+import pty
+import select
+import subprocess
+import struct
+import fcntl
+import termios
+
 from flask import Flask, Response, request, render_template, make_response, g, session
 from flask_restful import Resource, Api, fields
 from flask_cors import CORS
@@ -10,6 +18,8 @@ from flask_jwt_extended import JWTManager, get_jwt_identity, get_jwt_claims, get
 
 from flask_marshmallow import Marshmallow
 from flask_socketio import SocketIO, emit, send,join_room, leave_room
+import flask_socketio
+
 
 
 from flasgger import Swagger
@@ -190,6 +200,150 @@ except:
 socketio.on_namespace(DefaultSocketNamespace("/tasks"))
 
 
+def start_interpreter():
+    # create child process attached to a pty we can read from and write to
+    env = app.config['environment']
+    cmd = ['ipython', '-m', 'pytaskmanager.server.shell', '-i', '--', env]
+
+    master_fd, slave_fd = pty.openpty()
+    child = subprocess.Popen(
+        cmd, 
+        stdin=slave_fd, 
+        stdout=slave_fd, 
+        stderr=slave_fd
+    )
+    app.config["child"] = child
+    app.config["fd"] = master_fd
+    app.config["master_fd"] = master_fd
+    app.config["slave_fd"] = slave_fd
+
+    set_winsize(master_fd, 50, 50)
+
+    socketio.start_background_task(target=read_and_forward_pty_output)
+    print("ipython terminal backend")
+
+
+def assert_running_interpreter(start_if_required=False):
+
+    try:
+        if app.config["child"].poll() is None:
+            return True
+    except KeyError:
+        pass
+
+
+    if start_if_required:
+        start_interpreter()
+        return True
+
+    else:
+        if not 'socket_connections' in app.config:
+            print('This is weird!')
+            app.config['socket_connections'] = []
+
+        nr_clients = len(app.config['socket_connections'])
+
+        print('-' * 80)
+        print("The interpreter has been shut down.")
+        print("It will be restarted when a client reconnects.")
+        print()
+        if nr_clients == 1:
+            print(f'There is 1 client connected')
+        else:
+            print(f'There are {nr_clients} clients connected')
+        print('disconnecting ...')
+
+        for sid in app.config['socket_connections']:
+            with app.app_context():
+                print(f' - {sid}')
+                flask_socketio.disconnect(sid=sid, namespace='/pty')
+
+        print('-' * 80)
+
+        return False
+
+
+def set_winsize(fd, row, col, xpix=0, ypix=0):
+    winsize = struct.pack("HHHH", row, col, xpix, ypix)
+    fcntl.ioctl(fd, termios.TIOCSWINSZ, winsize)
+
+
+def read_and_forward_pty_output():
+    max_read_bytes = 1024 * 20
+
+    fd = app.config["fd"]
+    timeout_sec = 0.01
+
+    while assert_running_interpreter():
+        socketio.sleep(timeout_sec)
+
+        (rs, ws, es) = select.select([fd], [], [], timeout_sec)
+
+        for r in rs:
+            output = os.read(r, max_read_bytes).decode()
+            socketio.emit("pty-output", {"output": output}, namespace="/pty")
+
+
+@socketio.on("pty-input", namespace="/pty")
+def pty_input(data):
+    """write to the child pty. The pty sees this as if you are typing in a real
+    terminal.
+    """
+    if assert_running_interpreter():
+        raw_data = data["input"].encode()
+        os.write(app.config["fd"], raw_data)
+
+
+@socketio.on("resize", namespace="/pty")
+def resize(data):
+    if assert_running_interpreter():
+        set_winsize(app.config["fd"], data["rows"], data["cols"])
+
+
+@socketio.on("connect", namespace="/pty")
+def connect():
+    """new client connected"""
+    environment = app.config['environment']
+
+    print('-' * 80)
+    print('connect /pty')
+    print('environment: {environment}')
+    print('-' * 80)
+
+    try:
+        verify_jwt_in_request()
+    except Exception as e:
+        log.error("Could not connect client! No or Invalid JWT token?")
+        log.info(list(request.headers.keys()))
+        log.exception(e)
+    else:
+        # At this point we're sure that the user/client/whatever 
+        # checks out
+        user_or_node_id = get_jwt_identity()
+        auth = db.Authenticatable.get(user_or_node_id)
+
+        # define socket-session variables.
+        session.type = auth.type
+        session.name = auth.username if session.type == 'user' else auth.name
+        log.info(f'Client identified as <{session.type}>: <{session.name}>')
+
+        assert_running_interpreter(start_if_required=True)
+
+        if not 'socket_connections' in app.config:
+            app.config['socket_connections'] = []
+
+        print(f'connecting {request.sid}')
+        app.config['socket_connections'].append(request.sid)
+        return True
+
+    return False
+
+@socketio.on("disconnect", namespace="/pty")
+def disconnect():
+    print(f'Client {request.sid} disconnected')
+    app.config['socket_connections'].remove(request.sid)
+
+
 # ------------------------------------------------------------------------------
 # Resources / API's
 # ------------------------------------------------------------------------------
@@ -204,7 +358,7 @@ def load_resources(api, API_BASE, resources):
 # Http routes
 # ------------------------------------------------------------------------------
 @app.route(WEB_BASE+'/', defaults={'path': ''})
-@app.route(WEB_BASE+'/<path:path>')
+# @app.route(WEB_BASE+'/<path:path>')
 def index(path):
     return """
     <html>
@@ -237,28 +391,28 @@ def index(path):
 # ------------------------------------------------------------------------------
 # init & run
 # ------------------------------------------------------------------------------
-def init(environment=None, init_resources_=False):
-    """Initialize the server using a site-wide ServerContext."""
-    logging.getLogger("urllib3").setLevel(logging.WARNING)
+# def init(environment=None, init_resources_=False):
+#     """Initialize the server using a site-wide ServerContext."""
+#     logging.getLogger("urllib3").setLevel(logging.WARNING)
 
-    if environment is None:
-        if 'environment' in os.environ:
-            environment = os.environ['environment']
-        else:
-            environment = 'prod'
+#     if environment is None:
+#         if 'environment' in os.environ:
+#             environment = os.environ['environment']
+#         else:
+#             environment = 'prod'
 
-    # Load configuration and initialize logging system
-    ctx = util.ServerContext(APPNAME, 'default')
-    ctx.init(ctx.config_file, environment)
+#     # Load configuration and initialize logging system
+#     ctx = util.ServerContext(APPNAME, 'default')
+#     ctx.init(ctx.config_file, environment)
 
-    if init_resources_:
-        init_resources(ctx)
+#     if init_resources_:
+#         init_resources(ctx)
 
-    uri = ctx.get_database_location()
-    print('-' * 80)
-    print(uri)
-    print('-' * 80)
-    db.init(uri)
+#     uri = ctx.get_database_location()
+#     print('-' * 80)
+#     print(uri)
+#     print('-' * 80)
+#     db.init(uri)
 
 
 def init_resources(ctx):
@@ -305,6 +459,10 @@ def run(ctx, *args, **kwargs):
     if environment == 'test':
         log.warning("Setting 'JWT_ACCESS_TOKEN_EXPIRES' to one day!")
         app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
+
+    # print('-' * 80)
+    # print(app.root_path)
+    # print('-' * 80)
 
     # Actually start the server
     # app.run(*args, **kwargs)
