d89ca73c068535388317e9e5d2d1f4e91a0c11d6
Melle Sieswerda <melle.sieswerda@gmail.com>
2018-12-17 10:32:38 +0100

    Added support for multiple databases
    *This commit requires changes to the database!* 
    On the node side support is enabled by adding a key `databases` to the configuration file. Each entry below `databases` should contain key/value pairs for named databases and URIs. To make use of a specific database, tasks now have the attribute `database`. 
    
    Additionally:
    - class `NodeBase` was removed because it didn't add any value anymore.
    - Various spelling corrections.

--- vantage6-client/pytaskmanager/server/db.py
+++ vantage6-client/pytaskmanager/server/db.py
@@ -140,6 +140,40 @@ class Base(object):
         session.delete(self)
         session.commit()
 
+    def update(self, include=None, exclude=None, **kwargs):
+        """Update this instance using a dictionary."""
+
+        # Get a list of attributes available to this class.
+        # This should exclude relationships!
+        inst = inspect(self)
+        cols = [c_attr.key for c_attr in inst.mapper.column_attrs]
+        cols = set(cols)
+
+        # Cast the list of attributes we're trying to update to a set.
+        keys = set(kwargs.keys())
+
+        # Only *keep* keys listed in `include`
+        if include:
+            if type(include) != type([]):
+                include = [include, ]
+
+            include = set(include)
+            keys = keys & include
+
+        # Remove any keys that are in `exclude`
+        if exclude:
+            if type(exclude) != type([]):
+                exclude = [exclude, ]
+
+            exclude = set(exclude)
+            keys = keys - exclude
+
+        # Keep only those keys that are proper attributes
+        attrs = cols.intersection(keys)
+
+        for attr in attrs:
+            setattr(self, attr, kwargs[attr])
+
 
 Base = declarative_base(cls=Base)
 
@@ -283,6 +317,7 @@ class Node(Authenticatable):
     
     name = Column(String)
     api_key = Column(String)
+    state = Column(Text)
 
     collaboration_id = Column(Integer, ForeignKey('collaboration.id'))
     collaboration = relationship('Collaboration', backref='nodes')
@@ -316,8 +351,6 @@ class Node(Authenticatable):
             return None
 
 
-
-
 # ------------------------------------------------------------------------------
 class Task(Base):
     """Central definition of a single task."""
@@ -325,6 +358,7 @@ class Task(Base):
     description = Column(String)
     image = Column(String)
     input = Column(Text)
+    database = Column(String)
 
     collaboration_id = Column(Integer, ForeignKey('collaboration.id'))
     collaboration = relationship('Collaboration', backref='tasks')
