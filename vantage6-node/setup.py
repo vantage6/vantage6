51588493e142ead67a0514e903d4b5d9bac62f9c
fcmartin <frank@grafikus.nl>
2019-02-20 10:42:51 +0100

    fixed requirements syntax error

--- vantage6-node/setup.py
+++ vantage6-node/setup.py
@@ -56,7 +56,7 @@ setup(
         'requests',
         'termcolor',
         'sqlalchemy',
-        'package @ git+https://github.com/IKNL/flasgger@master' # flassger OAS 3+ support
+        'flasgger @ git+https://github.com/IKNL/flasgger.git' # flassger OAS 3+ support
     ],
     package_data={  
         'pytaskmanager': [
