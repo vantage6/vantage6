46e66e7f11784eecfa656f37f0b6a98701c33f0e
fcmartin <frank@grafikus.nl>
2019-03-08 12:13:03 +0100

    Updated UX for the node CLI. Questionaires are now invoked, see #22

--- vantage6/pytaskmanager/util/context.py
+++ vantage6/pytaskmanager/util/context.py
@@ -3,8 +3,107 @@ import sys
 import os
 import shutil
 import yaml
+import questionary as q
 
+from pathlib import Path
 from schema import Schema, And, Or, Use, Optional
+
+from pytaskmanager import APPNAME
+from pytaskmanager import util
+
+def node_configuration_questionaire(ctx):
+    """Questionary to generate a config file for the node instance."""
+
+    config = q.prompt([
+        {
+            "type": "text",
+            "name": "api_key",
+            "message": "Enter given api-key:"
+        },
+        {
+            "type": "text",
+            "name": "server_url",
+            "message": "The base-URL of the server:",
+            "default": "http://localhost"
+        },
+        {
+            "type": "text", 
+            "name": "port",
+            "message": "Enter port to which the server listens:",
+            "default": "5000"
+        },
+        {
+            "type": "text", 
+            "name": "api_path",
+            "message": "Path of the api:",
+            "default": "/api"
+        },
+        {
+            "type": "text", 
+            "name": "task_dir",
+            "message": "Task directory path:",
+            "default": ctx.data_dir
+        }
+    ])
+
+    config["databases"] = q.prompt([
+        {
+            "type": "text",
+            "name": "default",
+            "message": "Default database path:"
+        }
+    ])
+    i = 1
+    while q.confirm("Do you want to add another database?").ask():
+        q2 = q.prompt([
+        {
+            "type": "text",
+            "name": "label",
+            "message": "Enter the label for the database:",
+            "default": f"database_{i}"
+        },
+        {
+            "type": "text",
+            "name": "path",
+            "message": "The path of the database file:",
+            "default": str(
+                Path(config.get("databases").get("default")).parent)
+        }])
+        config["databases"][q2.get("label")] = q2.get("path")
+        i+=1
+
+
+    res = q.select("Which level of logging would you like?",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL", "NOTSET"]
+    ).ask()
+
+    config["logging"] = {
+        "level": res,
+        "file": f"{ctx.instance_name}.log",
+        "use_console": True,
+        "backup_count":5,
+        "max_size": 1024,
+        "format": "%(asctime)s - %(name)-14s - %(levelname)-8s - %(message)s",
+        "datefmt": "%H:%M:%S"
+    }
+
+    return {"application": config}
+
+def server_configuration_questionaire(ctx):
+    return {"application": None}
+
+def configuration_wizard(ctx, ex_cfg_file=None):
+    
+    if ctx.instance_type == "node":
+        config = node_configuration_questionaire(ctx)
+    elif ctx.instance_type == "server":
+        config = server_configuration_questionaire(ctx)
+    
+    cfg_file = ex_cfg_file if ex_cfg_file else ctx.config_file
+    with open(cfg_file, 'w') as f:
+        yaml.dump(config, f, default_flow_style=False)
+
+    return config
 
 def validate_configuration(configuration, instance_type):
     """Check that the configuration is valid for ppDLI.
@@ -14,18 +113,15 @@ def validate_configuration(configuration, instance_type):
     a single environment should be loaded. Therefore we only need 
     to validate the loaded configuration.
     """
-
-    # 
+     
     node_schema = {
         "api_key": And(Use(str), len),
         "server_url": Use(str),
         "port": Use(int),
-        "api_path": Use(str),
-        "server_entry_point": Use(str),
-        "delay": Use(int),
         "task_dir": Use(str),
-        "docker_host": Use(str),
-        "database_uri": os.path.exists
+        "databases": {Use(str):os.path.exists},
+        "api_path": Use(str),
+        Optional("logging"): Use(dict)
     }
     
     server_schema = {
@@ -36,8 +132,8 @@ def validate_configuration(configuration, instance_type):
         "api_path": Use(str),
         "uri": Use(str),
         "allow_drop_all": Use(bool),
-        "logging": {
-            "level": lambda l: l in ("DEBUG", "INFO", "WARNING", "CRITICAL"),
+        Optional("logging"): {
+            "level": lambda l: l in ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"),
             "file": Use(str),
             "use_console": Use(bool),
             "backup_count": And(Use(int),lambda n: n>0),
@@ -52,52 +148,22 @@ def validate_configuration(configuration, instance_type):
         'node': node_schema
     }.get(instance_type)
     schema = Schema(instance_schema, ignore_extra_keys=True)
-    
+
     return schema.validate(configuration)
 
+# TODO deprecated, still used by server instance, should be replaced
+# by some sort of questionaire
 def get_config_location(ctx, config, force_create):
     """Ensure configuration file exists and return its location."""
+    return config if config else ctx.config_file
     
-    if config is None:
-        # Get the location of config.yaml if not provided
-        filename = ctx.config_file
-    else:
-        # Use the config file provided as argument
-        filename = config
-
-    # Check that the config file exists and create it if necessary, but
-    # only if it was not explicitly provided!
-    if not os.path.exists(filename):
-        
-        # TODO let's remove this
-        if config and not force_create:
-            click.echo("Configuration file '{}' does not exist and '--force-create' not specified!".format(filename))
-            click.echo("Aborting ...")
-            sys.exit(1)
-
-        # make sure the config directory exists
-        dirname = os.path.dirname(filename)
-        if dirname:
-            os.makedirs(dirname, exist_ok=True)
-
-        # copy a default config file to the config location
-        skeleton_file = ctx.instance_type + "_config_skeleton.yaml"
-        src = os.path.join(ctx.package_data_dir(), skeleton_file)
-        dst = os.path.join(filename)
-        shutil.copy(src, dst)
-
-        # set the logging file to the name of the instance 
-        if ctx.instance_type == 'server':
-             # load current config
-            with open(dst, 'r') as fp:
-                cfg = yaml.load(fp)
-            
-            # update logging filename for all enviroments
-            for environment in cfg['environments'].items():
-                cfg[environment]['logging']['file'] = ctx.instance_name + '.log'
-
-            # write back to the config
-            with open(dst, 'w') as fp:
-                yaml.dump(cfg, fp)
-
-    return filename
+def select_configuration_questionaire(instance_type):
+    """Asks which configuration the user want to use
+    
+    It shows only configurations that are in the default folder.
+    """
+    ctx = util.AppContext(APPNAME, instance_type)
+    files = Path(ctx.config_dir).glob("*.yaml")
+    name = q.select("Select the configuration you want to use:",
+        choices=[file_.stem for file_ in files]).ask()
+    return name
