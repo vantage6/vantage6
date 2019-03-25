b5db0f0a4d4c208a946fcb4d633da708d58b8044
fcmartin <frank@grafikus.nl>
2019-03-25 14:48:35 +0100

    new cli command: `ppdev start` loads a fresh server with two organizations and launches their nodes. Making it easier to test algorithms for ppDLI

--- vantage6/pytaskmanager/server/controllers/fixture.py
+++ vantage6/pytaskmanager/server/controllers/fixture.py
@@ -35,11 +35,29 @@ def load(ctx, fixtures, drop_all=True):
         log.debug(f"processed collaboration={collaboration.name}")
 
         # append organizations to the collaboration
-        for name in col.get("participants",{}): 
-            collaboration.organizations.append(
-                db.Organization.get_by_name(name)
+    
+        for participant in col.get("participants",{}): 
+            
+            if isinstance(participant, str):
+                org_name = participant
+                node_api_key = str(uuid.uuid1())
+            elif isinstance(participant, dict):
+                org_name = participant.get("name")
+                node_api_key = participant.get("api-key", str(uuid.uuid1()))
+                
+            organization = db.Organization.get_by_name(org_name)
+            collaboration.organizations.append(organization)
+            log.debug(f"added {org_name} to the collaboration")
+
+            node = db.Node(
+                organization=organization, 
+                collaboration=collaboration,
+                name=f"{organization.name} - {collaboration.name} Node",
+                api_key=node_api_key
             )
-            log.debug(f"added {name} to the collaboration")
+            node.save()
+            log.debug(f"added node {node.name} to {collaboration.name}")
+            
         collaboration.save()
 
         # append dummy tasks to the collaboration
@@ -47,8 +65,14 @@ def load(ctx, fixtures, drop_all=True):
             task = db.Task(
                 name=f"Example task",
                 image=image,
-                collaboration=collaboration
+                collaboration=collaboration,
+                input="something"
             )
+            for node in collaboration.nodes:
+                task_result = db.TaskResult(
+                    node=node,
+                    task=task
+                )
             task.save()
             log.debug(f"Processed task {task.name}")
 
