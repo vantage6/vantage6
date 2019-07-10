46acd1a019344165da4621c6da8b02475c9cb066
fcmartin <frank@grafikus.nl>
2019-07-10 11:42:27 +0200

    Updated command line interface to dockerized node

---
+++ vantage6/joey/node/start.py
@@ -0,0 +1,30 @@
+import sys
+
+from joey import util, node
+
+def start(name, environment):
+    """Start the node instance.
+    
+    If no name or config is specified the default.yaml configuation is used. 
+    In case the configuration file not exists, a questionaire is
+    invoked to create one. Note that in this case it is not possible to
+    specify specific environments for the configuration (e.g. test, 
+    prod, acc). 
+    """
+    
+    # create context
+    ctx = util.DockerNodeContext(name, environment)
+
+    # run the node application
+    node.run(ctx)
+
+if __name__ == "__main__":
+
+    # configuration name
+    name = sys.argv[1]
+    
+    # environment in the config file (dev, test, acc, prod, application)
+    environment = sys.argv[2]
+
+    # run script to start
+    start(name, environment)
