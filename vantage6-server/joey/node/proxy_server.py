e7378829b2cc83db6b7324deb3585136220acc44
fcmartin <frank@grafikus.nl>
2019-07-17 15:16:20 +0200

    Added proxy server script

---
+++ vantage6-server/joey/node/proxy_server.py
@@ -0,0 +1,64 @@
+# app.run is only for development
+import requests
+import os
+
+from flask import Flask, request, jsonify
+from joey.node.server_io import ClientNodeProtocol
+
+app = Flask(__name__)
+
+def server_info():
+    """Retrieves environment variables set by the node.
+    """
+    url = os.environ["SERVER_URL"]
+    port = os.environ["SERVER_PORT"]
+    path = os.environ["SERVER_PATH"]
+    
+    assert url, "url not set in the environment variable"
+    # assert port, "port not set in the environment variable"
+    # assert path, "path not set in the environment variable"
+    return f"{url}:{port}{path}"
+
+def rest_method(method):
+    return {
+        'get': requests.get,
+        'post': requests.post,
+        'put': requests.put,
+        'patch': requests.patch,
+        'delete': requests.delete
+    }.get(method.lower(), 'get')
+
+@app.route('/')
+def info():
+    info = server_info()
+    return f'Hello, this is a proxy server for server: {info}'
+
+@app.route('/proxy/<path:central_server_path>')
+def proxy(central_server_path):
+    """ 
+    {
+        "method": "POST",
+        "endpoint": "/task/...",
+        "header": { ... }
+        "body": { ... }
+    }
+    """
+    return jsonify({"path":central_server_path, "method":request.method})
+
+    # data = request.get_json()
+    
+    # method = data.get("method")
+    # endpoint = data.get("endpoint")
+    # headers = data.get("header")
+    # params = data.get("params",None)
+    # json_body = data.get("json_body")
+    
+    # call = rest_method(method)
+    # full_server_url = server_info()
+    
+    # return call(
+    #     full_server_url+endpoint, 
+    #     json=json_body, 
+    #     headers=headers,
+    #     params=params
+    # )
