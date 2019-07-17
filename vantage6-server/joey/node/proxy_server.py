f78d490eac5f4f5eee5174b69e0c738890f85894
fcmartin <frank@grafikus.nl>
2019-07-17 16:45:26 +0200

    Cleaned up, added some doc

--- vantage6-server/joey/node/proxy_server.py
+++ vantage6-server/joey/node/proxy_server.py
@@ -1,4 +1,22 @@
-# app.run is only for development
+"""Proxy the communication from the algorithm to the central server.
+
+This module creates a tiny Flask app, to which the algorithm containers
+can make central server requests. Thereby limiting the connectivity of 
+the algorithm containers.
+
+Example:
+    
+
+Attributes: 
+    app (FlaskApp): contains the flask application
+
+Todo:
+    * bearer token from the node needs to be inserted
+    * limit the endpoints a container can reach, maybe add a arg and
+        check for it at the server
+    * encrypt input and result field
+    
+"""
 import requests
 import os
 
@@ -7,27 +25,16 @@ from joey.node.server_io import ClientNodeProtocol
 
 app = Flask(__name__)
 
-# TODO attach bearer header
-
 def server_info():
-    """Retrieves environment variables set by the node.
-    """
+    """Retrieves environment variables set by the node."""
     url = os.environ["SERVER_URL"]
     port = os.environ["SERVER_PORT"]
     path = os.environ["SERVER_PATH"]
-    
-    assert url, "url not set in the environment variable"
-    # assert port, "port not set in the environment variable"
-    # assert path, "path not set in the environment variable"
     return f"{url}:{port}{path}"
 
-@app.route('/')
-def info():
-    info = server_info()
-    return f'Hello, this is a proxy server for server: {info}'
-
-@app.route('/proxy/<path:central_server_path>')
+@app.route('/<path:central_server_path>')
 def proxy(central_server_path):
+    """Endpoint that will forward everything to the central server."""
     return jsonify({
         "path":central_server_path, 
         "method":request.method, 
@@ -38,7 +45,7 @@ def proxy(central_server_path):
 
 @app.route('/test/<path:central_server_path>')
 def test(central_server_path):
+    """Test endpoint, to be removed."""
     url = server_info()
-    # return f"{url}/{central_server_path}"
     response = requests.get(url+"/"+central_server_path)
     return jsonify(response.json())
