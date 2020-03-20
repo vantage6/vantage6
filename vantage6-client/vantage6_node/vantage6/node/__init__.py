1ea60ed32a48c4a8d7ba27f2d7ef3a9d4c478a39
Frank <frank@grafikus.nl>
2020-03-20 15:48:17 +0100

    Moved util folder into node package

--- vantage6-client/vantage6_node/vantage6/node/__init__.py
+++ vantage6-client/vantage6_node/vantage6/node/__init__.py
@@ -30,12 +30,12 @@ from threading import Thread
 from socketIO_client import SocketIO, SocketIONamespace
 from gevent.pywsgi import WSGIServer
 
-import vantage6.constants as cs
-
+import vantage6.node.constants as cs
+from vantage6.client import ServerInfo
 from vantage6.node.docker_manager import DockerManager
-from vantage6.node.server_io import ClientNodeProtocol, ServerInfo
+from vantage6.node.server_io import ClientNodeProtocol
 from vantage6.node.proxy_server import app 
-from vantage6.util import logger_name
+from vantage6.node.util import logger_name
 
 class NodeTaskNamespace(SocketIONamespace):
     """ Class that handles incoming websocket events.
