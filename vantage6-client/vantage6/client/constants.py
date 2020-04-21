24e3a97055924f3c6fc17b91959868f376f2004a
Melle Sieswerda <melle.sieswerda@gmail.com>
2020-04-22 00:05:03 +0200

    Fixed __version__ imports

--- vantage6-client/vantage6/client/constants.py
+++ vantage6-client/vantage6/client/constants.py
@@ -10,5 +10,5 @@ PACKAGE_FOLDER = Path(__file__).parent.parent.parent
 
 APPNAME = "vantage6"
 
-with open(Path(PACKAGE_FOLDER) / APPNAME / "client" / "VERSION") as f:
-    VERSION = f.read()
+# with open(Path(PACKAGE_FOLDER) / APPNAME / "client" / "VERSION") as f:
+#     VERSION = f.read()
