c39c5ba44c383bafba4099c43cc211a72b488425
Frank <frank@grafikus.nl>
2020-05-11 10:34:27 +0200

    bumped version

--- vantage6/setup.py
+++ vantage6/setup.py
@@ -43,8 +43,8 @@ setup(
         'questionary==1.5.1',
         'iPython==7.13.0',
         'SQLAlchemy==1.3.15',
-        'vantage6-common >= 1.0.0b7',
-        'vantage6-client >= 1.0.0b7',
+        'vantage6-common >= 1.0.0b12',
+        'vantage6-client >= 1.0.0b12',
     ],
     extras_require={
         'dev': [
