5a859f0f31d63473a047fcc703714d7c36ee5e9c
Frank <frank@grafikus.nl>
2020-09-02 14:51:30 +0200

    final version

--- vantage6-common/vantage6/common/_version.py
+++ vantage6-common/vantage6/common/_version.py
@@ -7,7 +7,7 @@ with open(os.path.join(here, '__build__')) as fp:
     __build__ = json.load(fp)
 
 # Module version
-version_info = (1, 1, 0, '', __build__)
+version_info = (1, 1, 0, 'final', __build__)
 
 # Module version stage suffix map
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
