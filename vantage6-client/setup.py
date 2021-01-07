87af86fe619b572dfeeeadfdc893575c03a2bafa
Frank <frank@grafikus.nl>
2021-01-07 15:33:59 +0100

    Frozen PyJWT to 1.7.1

--- vantage6-client/setup.py
+++ vantage6-client/setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires=[
        'cryptography==2.8',
        'requests==2.23.0',
-       'PyJWT',
+       'PyJWT==1.7.1',
        'pandas',
        'vantage6-common >= 1.2.3',
     ],
