b41b13508c96d8dcb3d591bcd9b12e2d09dd856c
Frank <frank@grafikus.nl>
2021-03-08 13:27:23 +0100

    Added default infrastructure name globals

--- vantage6-common/vantage6/common/globals.py
+++ vantage6-common/vantage6/common/globals.py
@@ -9,10 +9,14 @@ DEFAULT_ENVIRONMENT = "application"
 
 APPNAME = "vantage6"
 
+DEFAULT_DOCKER_REGISTRY = "harbor2.vantage6.ai"
+
+DEFAULT_NODE_IMAGE = "infrastructure/node:harukas"
+
+DEFAULT_SERVER_IMAGE = "infrastructure/server:harukas"
+
+
 #
 #   COMMON GLOBALS
 #
 PACAKAGE_FOLDER = Path(__file__).parent.parent.parent
-
-# with open(Path(PACAKAGE_FOLDER) / APPNAME / "common" / "VERSION") as f:
-#     VERSION = f.read()
