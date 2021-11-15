b533a0cb0e8d3bd586e7a557180a9b8e7ccb74ed
Frank Martin <f.martin@iknl.nl>
2021-11-15 10:41:01 +0100

    removed olf travis build files

--- vantage6-node/git-installs.sh
+++
@@ -1,6 +0,0 @@
-# install vantage6 dependancies from github on the same branch. This is usefull
-# as some test fail due to a vantage6-dependancy.
-pip install git+https://github.com/iknl/vantage6@$TRAVIS_BRANCH
-pip install git+https://github.com/iknl/vantage6-common@$TRAVIS_BRANCH
-pip install git+https://github.com/iknl/vantage6-client@$TRAVIS_BRANCH
-pip install git+https://github.com/iknl/vantage6-server@$TRAVIS_BRANCH
