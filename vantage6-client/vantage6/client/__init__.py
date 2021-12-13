43093586dc924fe1e839676a39d957a96709c135
Bart van Beusekom <b.vanbeusekom@iknl.nl>
2021-12-13 14:13:22 +0100

    Modified algorithm container client to handle multiple VPN ports

--- vantage6-client/vantage6/client/__init__.py
+++ vantage6-client/vantage6/client/__init__.py
@@ -1535,18 +1535,22 @@ class ContainerClient(ClientBase):
 
         return res
 
-    def get_other_node_ip_and_port(self, task_id: int):
+    def get_vpn_addresses(self, task_id: int):
         """
-        Return IP address and port number of other nodes involved in a task
-        so that VPN can be used for communication
+        Return IP address and port number of other algorithm containers
+        involved in a task so that VPN can be used for communication
         """
         results = self.request(f"task/{task_id}/result")
 
-        res = []
+        algorithm_addresses = []
         for result in results:
-            d = {'ip' : result['node']['ip'], 'port': result['port']}
-            res.append(d)
-        return res
+            for port in result['ports']:
+                algorithm_addresses.append({
+                    'ip': result['node']['ip'],
+                    'port': port['port'],
+                    'label': port['label']
+                })
+        return algorithm_addresses
 
     def get_task(self, task_id: int):
         return self.request(
