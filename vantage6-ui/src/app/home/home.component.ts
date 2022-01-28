fa64b8f36945e8b8c38a7d0cbc4c7803ad83f530
bart <b.vanbeusekom@iknl.nl>
2022-01-28 12:23:37 +0100

    Small updates

--- vantage6-ui/src/app/home/home.component.ts
+++ vantage6-ui/src/app/home/home.component.ts
@@ -1,18 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
-import { UserPermissionService } from '../services/user-permission.service';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  permissions: any[] = [];
+  constructor() {}
 
-  constructor(private userPermission: UserPermissionService) {}
-
-  ngOnInit() {
-    this.permissions = this.userPermission.getPermissions();
-  }
+  ngOnInit() {}
 }
