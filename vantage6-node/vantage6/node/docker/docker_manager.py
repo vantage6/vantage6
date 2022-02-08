b9f9f110a9d5c26d90e987ff53b4b00d1fc030ae
bart <b.vanbeusekom@iknl.nl>
2022-02-08 11:52:59 +0100

    Fix for multi-databases: ensure URI in docker does not contain the path on the host

--- vantage6-node/vantage6/node/docker/docker_manager.py
+++ vantage6-node/vantage6/node/docker/docker_manager.py
@@ -117,7 +117,8 @@ class DockerManager(DockerBaseManager):
         for label in db_labels:
             label_upper = label.upper()
             if running_in_docker():
-                uri = os.environ[f'{label_upper}_DATABASE_URI']
+                uri_env = os.environ[f'{label_upper}_DATABASE_URI']
+                uri = f'/mnt/{uri_env}'
             else:
                 uri = config['databases'][label]
 
@@ -126,8 +127,10 @@ class DockerManager(DockerBaseManager):
                 # We'll copy the file to the folder `data` in our task_dir.
                 self.log.info(f'Copying {uri} to {self.__tasks_dir}')
                 shutil.copy(uri, self.__tasks_dir)
+                uri = os.path.join(self.__tasks_dir, os.path.basename(uri))
 
             self.databases[label] = {'uri': uri, 'is_file': db_is_file}
+        self.log.debug(f"Databases: {self.databases}")
 
     def create_volume(self, volume_name: str) -> None:
         """
