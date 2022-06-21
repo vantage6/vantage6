821b27c81dd3fc30bc62de4246fd658a0547aecc
bart <b.vanbeusekom@iknl.nl>
2022-06-21 11:30:36 +0200

    New feature: Downloading algorithm logs

--- vantage6-ui/src/app/components/result/result-view/result-view.component.ts
+++ vantage6-ui/src/app/components/result/result-view/result-view.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Result, getEmptyResult } from 'src/app/interfaces/result';
 import { ApiResultService } from 'src/app/services/api/api-result.service';
+import { DownloadFileService } from 'src/app/services/common/download-file.service';
 import { ModalService } from 'src/app/services/common/modal.service';
 import { ResultDataService } from 'src/app/services/data/result-data.service';
 import { BaseViewComponent } from '../../base/base-view/base-view.component';
+import { ModalMessageComponent } from '../../modal/modal-message/modal-message.component';
 
 @Component({
   selector: 'app-result-view',
   templateUrl: './result-view.component.html',
-  styleUrls: ['./result-view.component.scss'],
+  styleUrls: [
+    '../../../shared/scss/buttons.scss',
+    './result-view.component.scss',
+  ],
 })
 export class ResultViewComponent extends BaseViewComponent implements OnInit {
   @Input() result: Result = getEmptyResult();
@@ -16,10 +21,22 @@ export class ResultViewComponent extends BaseViewComponent implements OnInit {
   constructor(
     protected apiResultService: ApiResultService,
     protected resultDataService: ResultDataService,
-    protected modalService: ModalService
+    protected modalService: ModalService,
+    private downloadFileService: DownloadFileService
   ) {
     super(apiResultService, resultDataService, modalService);
   }
 
   ngOnInit(): void {}
+
+  downloadLog(): void {
+    if (this.result.log) {
+      const filename = `logs_result_${this.result.id}.txt`;
+      this.downloadFileService.downloadTxtFile(this.result.log, filename);
+    } else {
+      this.modalService.openMessageModal(ModalMessageComponent, [
+        'Sorry, there log is empty!',
+      ]);
+    }
+  }
 }
