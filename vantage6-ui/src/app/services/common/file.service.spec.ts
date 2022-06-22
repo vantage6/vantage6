ad8f451f0045ec2677f5a6042a16fbc431686ffd
bart <b.vanbeusekom@iknl.nl>
2022-06-22 10:04:01 +0200

    Functionality to upload and download public keys

--- vantage6-ui/src/app/services/common/file.service.spec.ts
+++ vantage6-ui/src/app/services/common/file.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { DownloadFileService } from './download-file.service';
+import { FileService } from './file.service';
 
 describe('DownloadFileService', () => {
-  let service: DownloadFileService;
+  let service: FileService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(DownloadFileService);
+    service = TestBed.inject(FileService);
   });
 
   it('should be created', () => {
