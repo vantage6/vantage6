bb81aee6414217a65c863cd487d3d659ddeebbcc
Djura Smits <djura.smits@gmail.com>
2022-10-19 11:04:02 +0200

    Added additional dependencies

--- vantage6-client/setup.py
+++ vantage6-client/setup.py
@@ -40,7 +40,9 @@ setup(
         'pandas',
         f'vantage6-common=={version_ns["__version__"]}',
         'pyfiglet==0.8.post1',
-        'SPARQLWrapper==1.8.5'
+        'SPARQLWrapper==1.8.5',
+        'pyarrow',
+        'fastparquet'
     ],
     tests_require=["pytest"],
     package_data={
