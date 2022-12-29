9d5f940ae939b0a7e1e3eeb8c232cae91c76ae23
Frank Martin <frank@grafikus.nl>
2022-12-29 11:30:04 +0100

    added docstrings to the cli context

--- vantage6/vantage6/cli/context.py
+++ vantage6/vantage6/cli/context.py
@@ -1,4 +1,9 @@
+from __future__ import annotations
+
 import os.path
+
+from typing import Tuple
+from pathlib import Path
 
 from sqlalchemy.engine.url import make_url
 
@@ -14,22 +19,41 @@ from vantage6.cli._version import __version__
 
 
 class ServerContext(AppContext):
-    """ Context for the server.
+    """
+    Server context
 
-        Overwrites some methods from the AppContext. Also keeps track
-        of where the database is located.
+    Parameters
+    ----------
+    instance_name : str
+        Name of the configuration instance, corresponds to the filename
+        of the configuration file.
+    environment : str, optional
+        DTAP environment to load from the configuration file, by default
+        S_ENV
+    system_folders : bool, optional
+        System wide or user configuration, by default S_FOL
     """
 
+    # The server configuration manager is aware of the structure of the server
+    # configuration file and makes sure only valid configuration can be loaded.
     INST_CONFIG_MANAGER = ServerConfigurationManager
 
-    def __init__(self, instance_name, environment=S_ENV, system_folders=S_FOL):
+    def __init__(self, instance_name: str, environment: str = S_ENV,
+                 system_folders: bool = S_FOL):
         super().__init__("server", instance_name, environment=environment,
                          system_folders=system_folders)
         self.log.info(f"vantage6 version '{__version__}'")
 
-    def get_database_uri(self):
-        """ In the Docker environment we would like to overwrite the
-            uri setting by an environment variable.
+    def get_database_uri(self) -> str:
+        """
+        Obtain the database uri from the environment or the configuration. The
+        `VANTAGE6_DB_URI` environment variable is used by the Docker container,
+        but can also be set by the user.
+
+        Returns
+        -------
+        str
+            string representation of the database uri
         """
         uri = os.environ.get("VANTAGE6_DB_URI") or self.config['uri']
         URL = make_url(uri)
@@ -42,12 +66,40 @@ class ServerContext(AppContext):
         return uri
 
     @property
-    def docker_container_name(self):
+    def docker_container_name(self) -> str:
+        """
+        Unique name of the docker container.
+
+        Returns
+        -------
+        str
+            Unique docker container name
+        """
         return f"{APPNAME}-{self.name}-{self.scope}-server"
 
     @classmethod
-    def from_external_config_file(cls, path, environment=S_ENV,
-                                  system_folders=S_FOL):
+    def from_external_config_file(
+            cls, path: str, environment: str = S_ENV,
+            system_folders: bool = S_FOL) -> ServerContext:
+        """
+        Create a server context from an external configuration file. External
+        means that the configuration file is not located in the default folders
+        but its location is specified by the user.
+
+        Parameters
+        ----------
+        path : str
+            Path of the configuration file
+        environment : str, optional
+            DTAP environment to be loaded, by default S_ENV
+        system_folders : bool, optional
+            System wide or user configuration, by default S_FOL
+
+        Returns
+        -------
+        ServerContext
+            Server context object
+        """
         cls = super().from_external_config_file(
             path, "server", environment, system_folders
         )
@@ -58,87 +110,268 @@ class ServerContext(AppContext):
         return cls
 
     @classmethod
-    def config_exists(cls, instance_name, environment=S_ENV,
-                      system_folders=S_FOL):
+    def config_exists(cls, instance_name: str, environment: str = S_ENV,
+                      system_folders: bool = S_FOL) -> bool:
+        """
+        Check if a configuration file exists.
+
+        Parameters
+        ----------
+        instance_name : str
+            Name of the configuration instance, corresponds to the filename
+            of the configuration file.
+        environment : str, optional
+            DTAP environment that needs to be present, by default S_ENV
+        system_folders : bool, optional
+            System wide or user configuration, by default S_FOL
+
+        Returns
+        -------
+        bool
+            Whether the configuration file exists or not
+        """
         return super().config_exists("server", instance_name,
                                      environment=environment,
                                      system_folders=system_folders)
 
     @classmethod
-    def available_configurations(cls, system_folders=S_FOL):
+    def available_configurations(cls, system_folders: bool = S_FOL) \
+            -> Tuple[list, list]:
+        """
+        Find all available server configurations in the default folders.
+
+        Parameters
+        ----------
+        system_folders : bool, optional
+            System wide or user configuration, by default S_FOL
+
+        Returns
+        -------
+        Tuple[List, List]
+            The first list contains validated configuration files, the second
+            list contains invalid configuration files.
+        """
         return super().available_configurations("server", system_folders)
 
 
 class NodeContext(AppContext):
-    """Node context on the host machine (used by the CLI).
+    """
+    Node context
 
     See DockerNodeContext for the node instance mounts when running as a
     dockerized service.
+
+    Parameters
+    ----------
+    instance_name : str
+        Name of the configuration instance, corresponds to the filename
+        of the configuration file.
+    environment : str, optional
+        DTAP environment to be loaded, by default N_ENV
+    system_folders : bool, optional
+        _description_, by default N_FOL
+    config_file : str, optional
+        _description_, by default None
     """
 
-    # FIXME: drop the prefix "INST_": a *class* is assigned.
-    # FIXME: this does not need to be a class attribute, but ~~can~~_should_
-    #        be set in __init__
+    # The server configuration manager is aware of the structure of the server
+    # configuration file and makes sure only valid configuration can be loaded.
     INST_CONFIG_MANAGER = NodeConfigurationManager
 
+    # Flag to indicate if the node is running in a docker container or directly
+    # on the host machine.
     running_in_docker = False
 
-    def __init__(self, instance_name, environment=N_ENV,
-                 system_folders=N_FOL, config_file=None):
+    def __init__(self, instance_name: str, environment: str = N_ENV,
+                 system_folders: bool = N_FOL, config_file: str = None):
         super().__init__("node", instance_name, environment, system_folders,
                          config_file)
         self.log.info(f"vantage6 version '{__version__}'")
 
     @classmethod
-    def from_external_config_file(cls, path, environment=N_ENV,
-                                  system_folders=N_FOL):
+    def from_external_config_file(cls, path: str, environment: str = N_ENV,
+                                  system_folders: bool = N_FOL) -> NodeContext:
+        """
+        Create a node context from an external configuration file. External
+        means that the configuration file is not located in the default folders
+        but its location is specified by the user.
+
+        Parameters
+        ----------
+        path : str
+            Path of the configuration file
+        environment : str, optional
+            DTAP environment to be loaded, by default N_ENV
+        system_folders : bool, optional
+            System wide or user configuration, by default N_FOL
+
+        Returns
+        -------
+        NodeContext
+            Node context object
+        """
         return super().from_external_config_file(path, "node", environment,
                                                  system_folders)
 
     @classmethod
-    def config_exists(cls, instance_name, environment=N_ENV,
-                      system_folders=N_FOL):
+    def config_exists(cls, instance_name: str, environment: str = N_ENV,
+                      system_folders: bool = N_FOL) -> bool:
+        """
+        Check if a configuration file exists.
+
+        Parameters
+        ----------
+        instance_name : str
+            Name of the configuration instance, corresponds to the filename
+            of the configuration file.
+        environment : str, optional
+            DTAP environment that needs to be present, by default N_ENV
+        system_folders : bool, optional
+            System wide or user configuration, by default N_FOL
+
+        Returns
+        -------
+        bool
+            Whether the configuration file exists or not
+        """
         return super().config_exists("node", instance_name,
                                      environment=environment,
                                      system_folders=system_folders)
 
     @classmethod
-    def available_configurations(cls, system_folders=N_FOL):
+    def available_configurations(cls, system_folders: bool = N_FOL) \
+            -> Tuple[list, list]:
+        """
+        Find all available server configurations in the default folders.
+
+        Parameters
+        ----------
+        system_folders : bool, optional
+            System wide or user configuration, by default N_FOL
+
+        Returns
+        -------
+        Tuple[List, List]
+            The first list contains validated configuration files, the second
+            list contains invalid configuration files.
+        """
         return super().available_configurations("node", system_folders)
 
     @staticmethod
-    def type_data_folder(system_folders):
-        """Return OS specific data folder."""
+    def type_data_folder(system_folders: bool = N_FOL) -> Path:
+        """
+        Obtain OS specific data folder where to store node specific data.
+
+        Parameters
+        ----------
+        system_folders : bool, optional
+            System wide or user configuration, by default N_FOL
+
+        Returns
+        -------
+        Path
+            Path to the data folder
+        """
         return AppContext.type_data_folder("node", system_folders)
 
     @property
     def databases(self):
+        """
+        Dictionary of local databases that are available for this node.
+
+        Returns
+        -------
+        dict
+            dictionary with database names as keys and their corresponding
+            paths as values.
+        """
         return self.config["databases"]
 
     @property
-    def docker_container_name(self):
+    def docker_container_name(self) -> str:
+        """
+        Unique Docker container name of the node.
+
+        Returns
+        -------
+        str
+            Unique Docker container name
+        """
         return f"{APPNAME}-{self.name}-{self.scope}"
 
     @property
-    def docker_network_name(self):
+    def docker_network_name(self) -> str:
+        """
+        Private Docker network name which is unique for this node.
+
+        Returns
+        -------
+        str
+            Docker network name
+        """
         return f"{APPNAME}-{self.name}-{self.scope}-net"
 
     @property
-    def docker_volume_name(self):
+    def docker_volume_name(self) -> str:
+        """
+        Docker volume in which task data is stored. In case a file based
+        database is used, this volume contains the database file as well.
+
+        Returns
+        -------
+        str
+            Docker volume name
+        """
         return os.environ.get(
             'DATA_VOLUME_NAME',
             f"{self.docker_container_name}-vol"
         )
 
     @property
-    def docker_vpn_volume_name(self):
+    def docker_vpn_volume_name(self) -> str:
+        """
+        Docker volume in which the VPN configuration is stored.
+
+        Returns
+        -------
+        str
+            Docker volume name
+        """
         return os.environ.get(
             'VPN_VOLUME_NAME',
             f"{self.docker_container_name}-vpn-vol"
         )
 
-    def docker_temporary_volume_name(self, run_id):
+    def docker_temporary_volume_name(self, run_id: int) -> str:
+        """
+        Docker volume in which temporary data is stored. Temporary data is
+        linked to a specific run. Multiple algorithm containers can have the
+        same run id, and therefore the share same temporary volume.
+
+        Parameters
+        ----------
+        run_id : int
+            run id provided by the server
+
+        Returns
+        -------
+        str
+            Docker volume name
+        """
         return f"{APPNAME}-{self.name}-{self.scope}-{run_id}-tmpvol"
 
-    def get_database_uri(self, label="default"):
+    def get_database_uri(self, label: str = "default") -> str:
+        """
+        Obtain the database URI for a specific database.
+
+        Parameters
+        ----------
+        label : str, optional
+            Database label, by default "default"
+
+        Returns
+        -------
+        str
+            URI to the database
+        """
         return self.config["databases"][label]
