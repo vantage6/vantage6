6541f91426596056b8c6fb07b34da8cde026b313
bart <b.vanbeusekom@iknl.nl>
2023-03-15 16:22:06 +0100

    Rename initiator_id to init_org_id in v4

--- vantage6-ui/src/app/services/common/convert-json.service.ts
+++ vantage6-ui/src/app/services/common/convert-json.service.ts
@@ -183,7 +183,7 @@ export class ConvertJsonService {
       description: json.description,
       image: json.image,
       collaboration_id: json.collaboration.id,
-      initiator_id: json.initiator,
+      initiator_id: json.init_org,
       init_user_id: json.init_user,
       run_id: json.run_id,
       parent_id: json.parent ? json.parent.id : null,
