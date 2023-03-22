d14f1e678a06fbb1e087a85395431d012141336d
Frank Martin <f.martin@iknl.nl>
2023-03-22 15:22:20 +0100

    Settable config file location for the server

--- vantage6-server/server.sh
+++ vantage6-server/server.sh
@@ -1,4 +1,13 @@
+#!/bin/sh
+
 echo "[server.sh start]"
+
+# check if environment variable is set
+if [ -z "$VANTAGE6_CONFIG_LOCATION" ]; then
+    echo "VANTAGE6_CONFIG_LOCATION is not set"
+    echo "  using default location /mnt/config.yaml"
+    VANTAGE6_CONFIG_LOCATION="/mnt/config.yaml"
+fi
 
 uwsgi \
     --http :80 \
@@ -6,6 +15,6 @@ uwsgi \
     --http-websockets \
     --master --callable app --disable-logging \
     --wsgi-file /vantage6/vantage6-server/vantage6/server/wsgi.py \
-    --pyargv /mnt/config.yaml
+    --pyargv ${VANTAGE6_CONFIG_LOCATION}
 
 echo "[server.sh exit]"
