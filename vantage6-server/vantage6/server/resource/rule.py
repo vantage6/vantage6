1488359a330e03daef093cd48b734aa18f5601ec
Bart van Beusekom <b.vanbeusekom@iknl.nl>
2023-07-27 11:04:58 +0200

    Removed unused import

--- vantage6-server/vantage6/server/resource/rule.py
+++ vantage6-server/vantage6/server/resource/rule.py
@@ -13,7 +13,6 @@ from vantage6.server.resource import (
 )
 from vantage6.common import logger_name
 from vantage6.server import db
-from vantage6.server.permission import Operation as P
 from vantage6.server.resource.common.output_schema import RuleSchema
 from vantage6.server.resource.common.pagination import Pagination
 
