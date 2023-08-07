388e0025166bae0f0913b2ddfd5c3da5a22272ff
Richard Leurs <richard.leurs@elastique.nl>
2023-08-07 21:37:53 +0200

    Add navigation

--- vantage6-ui/new-frontend/src/app/app.module.ts
+++ vantage6-ui/new-frontend/src/app/app.module.ts
@@ -6,7 +6,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +31,11 @@ import { LayoutDefaultComponent } from './layouts/layout-default/layout-default.
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- vantage6-ui/new-frontend/src/app/layouts/layout-default/layout-default.component.ts
+++ vantage6-ui/new-frontend/src/app/layouts/layout-default/layout-default.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, ViewChild } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Subject, takeUntil } from 'rxjs';
+import { routePaths } from 'src/app/routes';
 
 @Component({
   selector: 'app-layout-default',
   templateUrl: './layout-default.component.html',
   styleUrls: ['./layout-default.component.scss']
 })
-export class LayoutDefaultComponent {}
+export class LayoutDefaultComponent implements AfterViewInit, OnDestroy {
+  destroy$ = new Subject();
+  hideSideMenu = false;
+  routes = routePaths;
+
+  @ViewChild(MatSidenav)
+  sideNav!: MatSidenav;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngAfterViewInit(): void {
+    this.breakpointObserver
+      .observe([Breakpoints.XSmall])
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((result) => {
+        this.hideSideMenu = result.matches;
+        if (!this.hideSideMenu) {
+          this.sideNav?.open();
+        } else {
+          this.sideNav?.close();
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+  }
+}
