dcd5007377f336a62ee8ed466d255e1d360d92f4
Richard Leurs <richard.leurs@elastique.nl>
2023-08-07 11:49:49 +0200

    Generate new Angular app

---
+++ vantage6-ui/new-frontend/src/app/app.component.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+    })
+  );
+});
