9406d81907a2fbb4bec13e3e9ba08efb12cb10f1
Bart van Beusekom <b.vanbeusekom@iknl.nl>
2023-09-14 15:37:08 +0200

    If a field is decrypted and decoded, first check if it is empty - if so, no decoding is done

--- vantage6-common/vantage6/common/client/client_base.py
+++ vantage6-common/vantage6/common/client/client_base.py
@@ -457,13 +457,17 @@ class ClientBase(object):
             Data on the algorithm run(s) with decrypted input
         """
         if is_single_resource:
-            data[field] = self._decrypt_input(data[field]).decode(
-                STRING_ENCODING)
+            if data.get(field):
+                data[field] = self._decrypt_input(
+                    data[field]
+                ).decode(STRING_ENCODING)
         else:
             # for multiple resources, data is in a 'data' field of a dict
             for resource in data['data']:
-                resource[field] = self._decrypt_input(resource[field]).decode(
-                    STRING_ENCODING)
+                if resource.get(field):
+                    resource[field] = self._decrypt_input(
+                        resource[field]
+                    ).decode(STRING_ENCODING)
 
         return data
 
