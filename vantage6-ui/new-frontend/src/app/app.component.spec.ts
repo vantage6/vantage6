2dfead2adecc00c739e863a8c0f1582ca151c8ef
Richard Leurs <richard.leurs@elastique.nl>
2023-10-31 10:02:58 +0100

    Update packages

--- vantage6-ui/new-frontend/src/app/app.component.spec.ts
+++ vantage6-ui/new-frontend/src/app/app.component.spec.ts
@@ -1,12 +1,22 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() =>
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent]
-    })
-  );
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
