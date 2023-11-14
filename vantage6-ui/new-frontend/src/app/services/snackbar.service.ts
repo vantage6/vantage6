589f36a70b56cf43010c9fc9d4af5953bb1aff20
Richard Leurs <richard.leurs@elastique.nl>
2023-11-14 15:01:10 +0100

    Translate snackbar close

--- vantage6-ui/new-frontend/src/app/services/snackbar.service.ts
+++ vantage6-ui/new-frontend/src/app/services/snackbar.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SnackbarService {
-  constructor(private snackBar: MatSnackBar) {}
+  constructor(
+    private snackBar: MatSnackBar,
+    private translateService: TranslateService
+  ) {}
 
   showMessage(message: string) {
-    this.snackBar.open(message, 'Close', {
+    this.snackBar.open(message, this.translateService.instant('general.close'), {
       duration: 20000 //Auto close after 20 seconds
     });
   }
