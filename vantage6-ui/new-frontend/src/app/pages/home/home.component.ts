9f59d0ed41e5f0074c3a2165d640ee19921d10b2
Bart van Beusekom <b.vanbeusekom@iknl.nl>
2023-12-04 15:00:36 +0100

    Add content to home page

--- vantage6-ui/new-frontend/src/app/pages/home/home.component.ts
+++ vantage6-ui/new-frontend/src/app/pages/home/home.component.ts
@@ -5,7 +5,8 @@ import { Version } from '../../models/api/version.model';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   serverUrl = environment.server_url;
