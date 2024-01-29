6bdb4ab198603b3a2b39ea54160a7a07a21c59d1
Bart van Beusekom <b.vanbeusekom@iknl.nl>
2024-01-29 11:32:50 +0100

    Remove code for old UI

--- vantage6-ui/src/assets/env.js
+++
@@ -1,9 +0,0 @@
-// The purpose of this file is to be able to insert environment variables from Docker
-(function(window) {
-    window["env"] = window["env"] || {};
-
-    // Environment variables
-    window["env"]["server_url"] = "https://cotopaxi.vantage6.ai";
-    window["env"]["api_path"] = "";
-    window["env"]["api_url"] = "https://cotopaxi.vantage6.ai";
-})(this);
