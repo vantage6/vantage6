431befa0ec2a6f861c9de440c6fe568e6b954f27
Bart van Beusekom <b.vanbeusekom@iknl.nl>
2024-01-30 17:13:58 +0100

    Added security headers to UI requests

---
+++ vantage6-ui/startup/replace_env_vars.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# replace environment variables for angular app
+envsubst < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
+
+# replace environment variables for nginx config. There the URL (without http(s))
+# is used in the Content-Security-Policy header.
+SERVER_URL_NO_HTTP="cotopaxi.vantage6.ai"
+if [ ! -z "${SERVER_URL}" ]; then
+    SERVER_URL_NO_HTTP=`echo $SERVER_URL | sed 's/^http[s]*:\/\///g'`
+fi
+sed -i "s/<SERVER_URL>/$SERVER_URL_NO_HTTP/g" /etc/nginx/nginx.conf
