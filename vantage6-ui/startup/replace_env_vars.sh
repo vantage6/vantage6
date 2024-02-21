61f08b98bb2a6d3554db76c28dc25ad52cf273ea
Bart van Beusekom <b.vanbeusekom@iknl.nl>
2024-02-21 11:59:51 +0100

    Address review comments for security headers

--- vantage6-ui/startup/replace_env_vars.sh
+++ vantage6-ui/startup/replace_env_vars.sh
@@ -5,14 +5,16 @@ envsubst < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/
 
 # replace environment variables for nginx config. There the URL (without http(s))
 # is used in the Content-Security-Policy header.
+# TODO the following process to set nginx configuration via sed is not ideal. Consider
+# doing it by directly using env vars in nginx.conf (see https://github.com/docker-library/docs/tree/master/nginx#using-environment-variables-in-nginx-configuration-new-in-119)
 SERVER_URL_NO_HTTP="cotopaxi.vantage6.ai"
 if [ ! -z "${SERVER_URL}" ]; then
-    SERVER_URL_NO_HTTP=`echo $SERVER_URL | sed 's/^http[s]*:\/\///g'`
+    SERVER_URL_NO_HTTP=`echo $SERVER_URL | sed 's/^https\?:\/\///g'`
 fi
 sed -i "s/<SERVER_URL>/$SERVER_URL_NO_HTTP/g" /etc/nginx/nginx.conf
 # also whitelist the allowed algorithm stores in the CSP header
 ALLOWED_ALGORITHM_STORES_NO_HTTP="*"
 if [ ! -z "${ALLOWED_ALGORITHM_STORES}" ]; then
-    ALLOWED_ALGORITHM_STORES_NO_HTTP=`echo $ALLOWED_ALGORITHM_STORES | sed 's/^http[s]*:\/\///g'`
+    ALLOWED_ALGORITHM_STORES_NO_HTTP=`echo $ALLOWED_ALGORITHM_STORES | sed 's/^https\?:\/\///g'`
 fi
 sed -i "s/<ALGORITHM_STORE_URLS>/$ALLOWED_ALGORITHM_STORES_NO_HTTP/g" /etc/nginx/nginx.conf
