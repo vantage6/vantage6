303e320b465ddfd9c267406341a86246e2d743bd
Bart van Beusekom <b.vanbeusekom@iknl.nl>
2024-03-06 10:57:08 +0100

    Update CSP policies
    - Put stuff into connect-src and font-src rather than default-src where possible
    - No longer cutting http[s] off of the URLs so that we prevent going over http where an https address is whitelisted

--- vantage6-ui/startup/replace_env_vars.sh
+++ vantage6-ui/startup/replace_env_vars.sh
@@ -7,14 +7,20 @@ envsubst < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/
 # is used in the Content-Security-Policy header.
 # TODO the following process to set nginx configuration via sed is not ideal. Consider
 # doing it by directly using env vars in nginx.conf (see https://github.com/docker-library/docs/tree/master/nginx#using-environment-variables-in-nginx-configuration-new-in-119)
-SERVER_URL_NO_HTTP="cotopaxi.vantage6.ai"
-if [ ! -z "${SERVER_URL}" ]; then
-    SERVER_URL_NO_HTTP=`echo $SERVER_URL | sed 's/^https\?:\/\///g'`
+if [ -z "${SERVER_URL}" ]; then
+    SERVER_URL="https://cotopaxi.vantage6.ai"
 fi
-sed -i "s/<SERVER_URL>/$SERVER_URL_NO_HTTP/g" /etc/nginx/nginx.conf
+# Remove http(s) from the server url
+SERVER_URL_NO_HTTP=$(echo $SERVER_URL | sed 's/^https\?:\/\///g')
+# escape the slashes in the url
+SERVER_URL=$(echo $SERVER_URL | sed 's/\//\\\//g')
+sed -i "s/<SERVER_URL>/$SERVER_URL/g" /etc/nginx/nginx.conf
+sed -i "s/<SERVER_URL_NO_HTTP>/$SERVER_URL_NO_HTTP/g" /etc/nginx/nginx.conf
+
 # also whitelist the allowed algorithm stores in the CSP header
-ALLOWED_ALGORITHM_STORES_NO_HTTP="*"
-if [ ! -z "${ALLOWED_ALGORITHM_STORES}" ]; then
-    ALLOWED_ALGORITHM_STORES_NO_HTTP=`echo $ALLOWED_ALGORITHM_STORES | sed 's/^https\?:\/\///g'`
+if [ -z "${ALLOWED_ALGORITHM_STORES}" ]; then
+    ALLOWED_ALGORITHM_STORES="*"
 fi
-sed -i "s/<ALGORITHM_STORE_URLS>/$ALLOWED_ALGORITHM_STORES_NO_HTTP/g" /etc/nginx/nginx.conf
+# escape the slashes in the urls
+ALLOWED_ALGORITHM_STORES=$(echo $ALLOWED_ALGORITHM_STORES | sed 's/\//\\\//g')
+sed -i "s/<ALGORITHM_STORE_URLS>/$ALLOWED_ALGORITHM_STORES/g" /etc/nginx/nginx.conf
