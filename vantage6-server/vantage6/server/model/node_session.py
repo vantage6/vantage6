45f3e8353000dd6beb2f0d86c3009890949f6825
F.C. Martin <f.martin@iknl.nl>
2024-03-20 16:07:40 +0100

    Added session status files

---
+++ vantage6-server/vantage6/server/model/node_session.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+from datetime import datetime, timezone
+from sqlalchemy import Column, Integer, ForeignKey, DateTime, Enum, UniqueConstraint
+from sqlalchemy.orm import relationship
+
+from vantage6.common.session_status import SessionStatus
+
+from vantage6.server.model.base import Base
+
+
+class NodeSession(Base):
+    """
+    Table that describes which sessions are available.
+
+    A session state keeps track of which nodes have prepared a session. When a
+    state is ready it can be used to execute computation tasks. A single session
+    contains multiple nodes, therefore a single session has multiple states.
+
+    Attributes
+    ----------
+    node : :class:`~vantage6.server.model.node.Node`
+        Node that has prepared the session
+    session : :class:`~vantage6.server.model.session.Session`
+        Session that is prepared by the node
+    state : SessionStatus
+        State of the session
+    last_updated_at : datetime.datetime
+        Date and time of the last update of the session state
+
+    Raises
+    ------
+    IntegrityError
+        If the node has a status for a specific session
+    """
+
+    # fields
+    node_id = Column(Integer, ForeignKey("node.id"))
+    session_id = Column(Integer, ForeignKey("session.id"))
+    state = Column(Enum(SessionStatus), default=SessionStatus.PENDING)
+    last_updated_at = Column(DateTime, default=datetime.now(timezone.utc))
+
+    __table_args__ = (UniqueConstraint("node_id", "session_id"), )
+
+    # relationships
+    node = relationship("Node", back_populates="sessions")
+    session = relationship("Session", back_populates="node_sessions")
+    config = relationship("NodeSessionConfig", back_populates="node_session")
+
+    def __repr__(self):
+        """
+        Returns a string representation of the session state.
+
+        Returns
+        -------
+        str
+            String representation of the session state
+        """
+        return (
+            f"<NodeSession {self.id}, "
+            f"node_id={self.node_id}, "
+            f"session_id={self.session_id}, "
+            f"state={self.state}, "
+            ">"
+        )
---
+++ vantage6-server/vantage6/server/model/session.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+
+from datetime import datetime, timezone
+from sqlalchemy import (Column, String, Integer, ForeignKey, DateTime, exists,
+                        UniqueConstraint)
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.hybrid import hybrid_property
+
+from vantage6.common.session_status import SessionStatus
+
+from vantage6.server.model.base import Base
+from vantage6.server.model.base import DatabaseSessionManager
+
+
+class Session(Base):
+    """
+    Table that describes which sessions are available.
+
+    Sessions allow for users to create a state at the node. This state can be used
+    to store data that exchanged between algorithm containers. A session is part of a
+    collaboration and optionally linked to a specific study.
+
+    Attributes
+    ----------
+    label : str
+        Label of the session
+    owner : :class:`~vantage6.server.model.user.User`
+        User that owns the session
+    collaboration : :class:`~vantage6.server.model.collaboration.Collaboration`
+        Collaboration that this session is part of
+    created_at : datetime.datetime
+        Date and time of the creation of the session
+    last_used_at : datetime.datetime
+        Date and time of the last usage of the session
+    tasks : list[:class:`~vantage6.server.model.task.Task`]
+        List of tasks that are part of this study
+
+    Raises
+    ------
+    IntegrityError
+        If the label already exists within the collaboration
+    """
+
+    # fields
+    label = Column(String)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    collaboration_id = Column(Integer, ForeignKey("collaboration.id"))
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
+    last_used_at = Column(DateTime, default=datetime.now(timezone.utc))
+
+    __table_args__ = (UniqueConstraint("label", "collaboration_id"), )
+
+    # relationships
+    owner = relationship("User", back_populates="sessions")
+    collaboration = relationship("Collaboration", back_populates="sessions")
+    tasks = relationship("Task", back_populates="session")
+    node_sessions = relationship("NodeSession", back_populates="session")
+
+    @property
+    def is_ready(self):
+        """
+        Check if the session is ready to be used.
+
+        Returns
+        -------
+        bool
+            True if the session is ready, False otherwise
+        """
+        return all(state.state == SessionStatus.READY for state in self.states)
+
+    def __repr__(self):
+        """
+        Returns a string representation of the session.
+
+        Returns
+        -------
+        str
+            String representation of the session
+        """
+        number_of_tasks = len(self.tasks)
+        return (
+            "<Session "
+            f"{self.id}: '{self.label}', "
+            f"owner_id={self.user_id}, "
+            f"collaboration_id={self.collaboration_id}, "
+            f"{number_of_tasks} task(s)"
+            ">"
+        )
