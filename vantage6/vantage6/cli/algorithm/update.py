8d0f56a5ded5c357ac85ccd4225f7bfeea5fe8c9
Bart van Beusekom <b.vanbeusekom@iknl.nl>
2024-03-28 16:48:43 +0100

    Minor updates to algorithm template generation
    - Updated copier dependency
    - New behaviour in 'v6 algorithm update' to skip questions previously answered
    - New flag in 'v6 algorithm update' to allow user to change questions previously answered
    - Using copier '--unsafe' flag in 'v6 algorithm create' and 'update' to be able to use new Python cleanup script defined in template creator

--- vantage6/vantage6/cli/algorithm/update.py
+++ vantage6/vantage6/cli/algorithm/update.py
@@ -4,8 +4,9 @@ from pathlib import Path
 import click
 import questionary as q
 from copier import run_update
+import copier.errors
 
-from vantage6.cli.utils import info
+from vantage6.common import info, warning
 
 
 @click.command()
@@ -17,7 +18,14 @@ from vantage6.cli.utils import info
     type=str,
     help="Directory to put the algorithm into",
 )
-def cli_algorithm_update(directory: str) -> dict:
+@click.option(
+    "--change-answers",
+    is_flag=True,
+    flag_value=True,
+    help="Change answers to questions that were already answered",
+    default=False,
+)
+def cli_algorithm_update(directory: str, change_answers: bool) -> dict:
     """Update an algorithm template
 
     When a new version of the algorithm template is released, you can update
@@ -28,6 +36,11 @@ def cli_algorithm_update(directory: str) -> dict:
         default_dir = str(Path(os.getcwd()))
         directory = q.text("Algorithm directory:", default=default_dir).ask()
 
-    run_update(directory, overwrite=True)
-
-    info("Template updated!")
+    info("Updating template...")
+    try:
+        run_update(
+            directory, overwrite=True, skip_answered=not change_answers, unsafe=True
+        )
+        info("Template updated!")
+    except copier.errors.UserMessageError as exc:
+        warning(f"Update failed: {exc}")
