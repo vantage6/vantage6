c2b0f2de510061f139bd0e7f9f5dceb853bf976a
Bart van Beusekom <b.vanbeusekom@iknl.nl>
2024-04-17 16:42:37 +0200

    Change default log level of client to info

--- vantage6-client/vantage6/client/__init__.py
+++ vantage6-client/vantage6/client/__init__.py
@@ -32,7 +32,7 @@ module_name = __name__.split(".")[1]
 class UserClient(ClientBase):
     """User interface to the vantage6-server"""
 
-    def __init__(self, *args, log_level="debug", **kwargs) -> None:
+    def __init__(self, *args, log_level="info", **kwargs) -> None:
         """Create user client
 
         All paramters from `ClientBase` can be used here.
@@ -40,7 +40,7 @@ class UserClient(ClientBase):
         Parameters
         ----------
         log_level : str, optional
-            The log level to use, by default 'debug'
+            The log level to use, by default 'info'
         """
         super(UserClient, self).__init__(*args, **kwargs)
 
