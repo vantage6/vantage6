3caed5161ca51971056bea58a2df659c27cf0172
Frank <mail@franky.codes>
2024-05-15 16:31:56 +0200

    Removed the logic for username reset password

--- vantage6-ui/src/app/pages/auth/password-lost/password-lost.component.ts
+++ vantage6-ui/src/app/pages/auth/password-lost/password-lost.component.ts
@@ -9,46 +9,38 @@ import { LoginErrorService } from 'src/app/services/login-error.service';
   templateUrl: './password-lost.component.html',
   styleUrls: ['./password-lost.component.scss']
 })
+
 export class PasswordLostComponent implements OnInit {
-  private emailOrUsername(group: FormGroup): ValidationErrors | null {
-    const email = group.controls['email'].value;
-    const username = group.controls['username'].value;
-    const hasAtLeastOne = email || username;
-    return hasAtLeastOne
-      ? null
-      : {
-          emailOrUsername: true
-        };
-  }
-
-  forgotPasswordForm = this.fb.group(
-    {
-      username: [''],
-      email: ['']
-    },
-    { validator: this.emailOrUsername }
-  );
-  executed_request = false;
-  responseMsg = '';
-  routes = routePaths;
-
-  constructor(
-    public loginErrorService: LoginErrorService,
-    private fb: FormBuilder,
-    private authService: AuthService
-  ) {}
-
-  ngOnInit(): void {
-    this.loginErrorService.clearError();
-  }
-
-  async onSubmit(): Promise<void> {
-    if (!this.forgotPasswordForm.valid) return;
-
-    const responseMsg = await this.authService.passwordLost(this.forgotPasswordForm.value);
-    if (responseMsg) {
-      this.executed_request = true;
-      this.responseMsg = responseMsg;
+    private email(group: FormGroup): ValidationErrors | null {
+      const email = group.controls['email'].value;
+      return email ? null : { email: true };
     }
-  }
+
+    forgotPasswordForm = this.fb.group(
+      { email: [''] },
+      { validator: this.email }
+    );
+
+    executed_request = false;
+    responseMsg = '';
+    routes = routePaths;
+
+    constructor(
+      public loginErrorService: LoginErrorService,
+      private fb: FormBuilder,
+      private authService: AuthService
+    ) {}
+
+    ngOnInit(): void {
+      this.loginErrorService.clearError();
+    }
+
+    async onSubmit(): Promise<void> {
+      if (!this.forgotPasswordForm.valid) return;
+      const responseMsg = await this.authService.passwordLost(this.forgotPasswordForm.value);
+      if (responseMsg) {
+        this.executed_request = true;
+        this.responseMsg = responseMsg;
+      }
+    }
 }
