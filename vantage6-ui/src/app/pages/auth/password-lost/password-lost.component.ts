f37e129f5390e588c933cf03b708c24efb03b55e
Bart van Beusekom <b.vanbeusekom@iknl.nl>
2024-05-22 14:29:28 +0200

    Using Prettier to format ts

--- vantage6-ui/src/app/pages/auth/password-lost/password-lost.component.ts
+++ vantage6-ui/src/app/pages/auth/password-lost/password-lost.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { routePaths } from 'src/app/routes';
 import { AuthService } from 'src/app/services/auth.service';
 import { LoginErrorService } from 'src/app/services/login-error.service';
@@ -9,36 +9,34 @@ import { LoginErrorService } from 'src/app/services/login-error.service';
   templateUrl: './password-lost.component.html',
   styleUrls: ['./password-lost.component.scss']
 })
-
 export class PasswordLostComponent implements OnInit {
-    forgotPasswordForm = new FormGroup({
-        email: new FormControl('', [Validators.required, Validators.email])
-      });
+  forgotPasswordForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email])
+  });
 
+  executed_request = false;
+  responseMsg = '';
+  routes = routePaths;
 
-    executed_request = false;
-    responseMsg = '';
-    routes = routePaths;
+  constructor(
+    public loginErrorService: LoginErrorService,
+    private authService: AuthService
+  ) {}
 
-    constructor(
-      public loginErrorService: LoginErrorService,
-      private authService: AuthService
-    ) {}
+  ngOnInit(): void {
+    this.loginErrorService.clearError();
+  }
 
-    ngOnInit(): void {
-      this.loginErrorService.clearError();
-    }
+  async onSubmit(): Promise<void> {
+    if (!this.forgotPasswordForm.valid) return;
 
-    async onSubmit(): Promise<void> {
-        if (!this.forgotPasswordForm.valid) return;
-
-        const emailValue = this.forgotPasswordForm.get('email')?.value;
-        if (emailValue) {
-          const responseMsg = await this.authService.passwordLost({ email: emailValue });
-          if (responseMsg) {
-            this.executed_request = true;
-            this.responseMsg = responseMsg;
-          }
-        }
+    const emailValue = this.forgotPasswordForm.get('email')?.value;
+    if (emailValue) {
+      const responseMsg = await this.authService.passwordLost({ email: emailValue });
+      if (responseMsg) {
+        this.executed_request = true;
+        this.responseMsg = responseMsg;
       }
+    }
+  }
 }
