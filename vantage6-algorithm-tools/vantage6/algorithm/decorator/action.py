d3555c11a7a0c6bfa8a89035b425994aebb01183
F.C. Martin <f.martin@iknl.nl>
2024-07-02 17:43:00 +0200

    WIP

--- vantage6-algorithm-tools/vantage6/algorithm/decorator/action.py
+++ vantage6-algorithm-tools/vantage6/algorithm/decorator/action.py
@@ -2,21 +2,24 @@ import os
 import pyarrow as pa
 import pandas as pd
 
+from datetime import datetime
 from functools import wraps
 
 from vantage6.common import error, debug
 from vantage6.common.enums import LocalAction
+from vantage6.algorithm.decorators import
 from vantage6.algorithm.tools.exceptions import DataTypeError, SessionError
 
 
 def data_extraction(func: callable) -> callable:
     # v Verify that the container has been started as data_extraction
-    # - Store return DataFrame in a parquet file
-    # - Write some other status statement in the output
-    # - Update session log
+    # v Store return DataFrame in a parquet file
+    # v Write some other status statement in the output
+    # v Update session log
+    # - report column names and types to the server
     @wraps(func)
+
     def wrapper(*args, **kwargs) -> None:
-        debug("Verifying container action.")
         exit_if_action_mismatch(LocalAction.DATA_EXTRACTION)
 
         result = func(*args, **kwargs)
@@ -29,31 +32,71 @@ def data_extraction(func: callable) -> callable:
 
         # TODO give a sensible name to the parquet file
         debug("Writing data to parquet file.")
-        pa.Table.from_pandas(result).write_table("data.parquet")
+        try:
+            pa.Table.from_pandas(result).write_table("data_extraction_result.parquet")
+        except Exception as e:
+            error(f"Error writing data to parquet file: {e}")
+            raise
 
-        debug("Updating session state.")
-        if _session_state_file_exists():
-            debug(
-                "Session state file already exists. Which should not be the case as the "
-                "data extraction step is the first step in the workflow. Exiting."
-            )
-            raise SessionError("Session state file already exists.")
+        _raise_if_session_already_exists()
         _create_session_state_file()
 
-        # TODO report column names at this point ?
+        _set_state(
+            "data-extraction",
+            "data_extraction_result.parquet",
+            "Data extraction complete.",
+        )
+
+        # TODO report column names
+        # TODO report the column types
+        # obtain token
+        # create algorithm client
+        # post to the session endpoint
+
+        # Nothing is returned from the data-extraction container to the vantage6 server
         return
 
     return wrapper
 
 
+def _set_state(action: str, file: str, message: str):
+    debug("Updating session state.")
+    try:
+        state = pa.parquet.read_table("session_state.parquet").to_pandas()
+        state = state.append(
+            {
+                "step": "data-extraction",
+                "action": action,
+                "file": file,
+                "timestamp": datetime.now(),
+                "message": message,
+            },
+            ignore_index=True,
+        )
+        pa.Table.from_pandas(state).write_table("session_state.parquet")
+    except Exception as e:
+        error(f"Error when updating session state: {e}")
+        raise SessionError("Error when updating session state.")
+
+
 def _create_session_state_file():
-    pa.Table.from_pandas(
-        pd.DataFrame(columns=["step", "action", "file", "timestamp", "message"])
-    ).write_table("session_state.parquet")
+    debug("Creating local session file.")
+    try:
+        pa.Table.from_pandas(
+            pd.DataFrame(columns=["step", "action", "file", "timestamp", "message"])
+        ).write_table("session_state.parquet")
+    except Exception as e:
+        error(f"Error when creating local session file.")
+        raise SessionError("Error when creating local session file.")
 
 
-def _session_state_file_exists():
-    return os.path.exists("session_state.parquet")
+def _raise_if_session_already_exists():
+    if os.path.exists("session_state.parquet"):
+        debug(
+            "Session state file already exists. Which should not be the case as the "
+            "data extraction step is the first step in the workflow. Exiting."
+        )
+        raise SessionError("Session state file already exists.")
 
 
 def pre_processing(func: callable):
@@ -96,7 +139,7 @@ def central(func: callable):
 
 
 def exit_if_action_mismatch(requested_action: LocalAction):
-
+    debug("Verifying container action.")
     if "CONTAINER_ACTION" not in os.environ:
         error("Container action not set. Exiting.")
         exit(1)
