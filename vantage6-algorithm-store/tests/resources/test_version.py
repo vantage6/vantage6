8a787289d1932429b03797a35277be59fa109283
Bart van Beusekom <b.vanbeusekom@iknl.nl>
2024-07-03 15:12:04 +0200

    Basic setup of algorithm store unit tests

---
+++ vantage6-algorithm-store/tests/resources/test_version.py
@@ -0,0 +1,20 @@
+import unittest
+from unittest.mock import patch
+import json
+
+from vantage6.algorithm.store import __version__
+
+from ..base.unittest_base import TestResources
+
+
+class TestAlgorithmResources(TestResources):
+
+    def test_version(self):
+        rv = self.app.get("/api/version")
+        r = json.loads(rv.data)
+        self.assertIn("version", r)
+        self.assertEqual(r["version"], __version__)
+
+
+if __name__ == "__main__":
+    unittest.main()
