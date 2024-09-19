f19bc666c79eaee96ace6588fbf8391a90215991
Bart van Beusekom <b.vanbeusekom@iknl.nl>
2024-09-19 16:18:15 +0200

    Adding rule to receive socket events on collaboration level to viewer, researcher and organization admin role
    This means they will be able to see task progress of other nodes in their collaboration

--- vantage6-server/vantage6/server/default_roles.py
+++ vantage6-server/vantage6/server/default_roles.py
@@ -65,6 +65,7 @@ def get_default_roles(db) -> list[dict]:
         db.Rule.get_by_("run", Scope.COLLABORATION, Operation.VIEW),
         db.Rule.get_by_("port", Scope.ORGANIZATION, Operation.VIEW),
         db.Rule.get_by_("event", Scope.ORGANIZATION, Operation.RECEIVE),
+        db.Rule.get_by_("event", Scope.COLLABORATION, Operation.RECEIVE),
         db.Rule.get_by_("study", Scope.ORGANIZATION, Operation.VIEW),
     ]
     VIEWER_ROLE = {
@@ -121,7 +122,6 @@ def get_default_roles(db) -> list[dict]:
         db.Rule.get_by_("node", Scope.COLLABORATION, Operation.CREATE),
         db.Rule.get_by_("node", Scope.COLLABORATION, Operation.VIEW),
         db.Rule.get_by_("node", Scope.COLLABORATION, Operation.DELETE),
-        db.Rule.get_by_("event", Scope.COLLABORATION, Operation.RECEIVE),
         db.Rule.get_by_("event", Scope.COLLABORATION, Operation.SEND),
         db.Rule.get_by_("study", Scope.COLLABORATION, Operation.VIEW),
         db.Rule.get_by_("study", Scope.COLLABORATION, Operation.CREATE),
