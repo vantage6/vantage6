bda9fb49527a8a55811ec7c0f060bebdc2702994
Bart van Beusekom <b.vanbeusekom@iknl.nl>
2024-12-09 16:39:58 +0100

    Only show running algorithm stores in v6 algorithm-store attach, instead of all available configurations

--- vantage6/vantage6/cli/algostore/attach.py
+++ vantage6/vantage6/cli/algostore/attach.py
@@ -1,19 +1,23 @@
 import click
 import docker
+import questionary as q
 
 from colorama import Fore, Style
 
 from vantage6.common import error
 from vantage6.common.docker.addons import check_docker_running
 from vantage6.common.globals import APPNAME, InstanceType
-from vantage6.cli.common.decorator import click_insert_context
 from vantage6.cli.common.start import attach_logs
-from vantage6.cli.context.algorithm_store import AlgorithmStoreContext
+from vantage6.cli.globals import DEFAULT_SERVER_SYSTEM_FOLDERS
 
 
 @click.command()
-@click_insert_context(InstanceType.ALGORITHM_STORE)
-def cli_algo_store_attach(ctx: AlgorithmStoreContext) -> None:
+@click.option("-n", "--name", default=None, help="configuration name")
+@click.option("--system", "system_folders", flag_value=True)
+@click.option(
+    "--user", "system_folders", flag_value=False, default=DEFAULT_SERVER_SYSTEM_FOLDERS
+)
+def cli_algo_store_attach(name: str, system_folders: bool) -> None:
     """
     Show the server logs in the current console.
     """
@@ -25,8 +29,17 @@ def cli_algo_store_attach(ctx: AlgorithmStoreContext) -> None:
     )
     running_server_names = [container.name for container in running_servers]
 
-    if ctx.docker_container_name in running_server_names:
-        container = client.containers.get(ctx.docker_container_name)
+    if not name:
+        name = q.select(
+            "Select the algorithm store you wish to inspect:",
+            choices=running_server_names,
+        ).ask()
+    else:
+        post_fix = "system" if system_folders else "user"
+        name = f"{APPNAME}-{name}-{post_fix}-{InstanceType.SERVER}"
+
+    if name in running_server_names:
+        container = client.containers.get(name)
         attach_logs(container, InstanceType.ALGORITHM_STORE)
     else:
-        error(f"{Fore.RED}{ctx.name}{Style.RESET_ALL} is not running!")
+        error(f"{Fore.RED}{name}{Style.RESET_ALL} is not running!")
