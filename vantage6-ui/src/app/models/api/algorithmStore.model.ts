4f4bfec93a3ac653eab83a7b9a154be671c4466c
Bart van Beusekom <b.vanbeusekom@iknl.nl>
2025-01-15 14:00:07 +0100

    Prevent racing condition in loading non-public policies for algorithm store are displayed on store page, and properly display the new policies there

--- vantage6-ui/src/app/models/api/algorithmStore.model.ts
+++ vantage6-ui/src/app/models/api/algorithmStore.model.ts
@@ -41,7 +41,12 @@ export enum AlgorithmStoreLazyProperties {
 export enum AvailableStorePolicies {
   ALGORITHM_VIEW = 'algorithm_view',
   ALLOWED_SERVERS = 'allowed_servers',
-  ALLOW_LOCALHOST = 'allow_localhost'
+  ALLOW_LOCALHOST = 'allow_localhost',
+  MIN_REVIEWERS = 'min_reviewers',
+  ASSIGN_REVIEW_OWN_ALGORITHM = 'assign_review_own_algorithm',
+  MIN_REVIEWING_ORGANIZATIONS = 'min_reviewing_organizations',
+  ALLOWED_REVIEWERS = 'allowed_reviewers',
+  ALLOWED_REVIEW_ASSIGNERS = 'allowed_review_assigners'
 }
 export interface StorePolicies {
   // TODO it would be nice if we could have a more specific type here like
