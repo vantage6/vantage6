24314bb1f6e4f7353af02517668fab1d03d3b3b0
F.C. Martin <f.martin@iknl.nl>
2025-01-16 15:40:07 +0100

    Remove legacy node and Kubernetes poc node files and configurations

--- vantage6-k8s-node/vantage6/k8s_node/pod_job_constants.py
+++
@@ -1,7 +0,0 @@
-
-# Paths within the Job POD-containers (the ones running the algorithms) defined by convention
-
-JOB_POD_INPUT_PATH = '/app/output'
-JOB_POD_OUTPUT_PATH = '/app/input'
-JOB_POD_TOKEN_PATH = '/app/token'
-JOB_POD_TMP_FOLDER_PATH = '/app/tmp'
--- vantage6-k8s-node/vantage6/k8s_node/pod_node_constants.py
+++
@@ -1,8 +0,0 @@
-# Paths within the Node POD-container defined by convention - these must match the mountPaths on kubeconfs/node_pod_config.yaml
-
-TASK_FILES_ROOT = '/app/tasks'
-KUBE_CONFIG_FILE_PATH = '/app/.kube/config'  
-V6_NODE_CONFIG_FILE = '/app/.v6node/configs/node_legacy_config.yaml'
-V6_NODE_DATABASE_BASE_PATH = '/app/.databases/'
-V6_NODE_FQDN = 'http://v6proxy-subdomain.v6-jobs.svc.cluster.local' # Must be consistent with kubeconfs/node_pod_config.yaml
-V6_NODE_PROXY_PORT = 4567
