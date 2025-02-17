d5f11ea5384a274ffc6bf156bc4a80501fc4abc1
Frank <mail@franky.codes>
2025-02-17 08:40:41 +0100

    Update node deployment path and add commented example for session algorithm

--- dev/connect-store.py
+++ dev/connect-store.py
@@ -19,3 +19,38 @@ if not local_store_url in existing_urls:
         all_collaborations=True,
         force=True,  # required to link localhost store
     )
+
+
+# client.algorithm.create(
+#     name="session basic example",
+#     description="A basic example of a session algorithm",
+#     image="harbor2.vantage6.ai/algorithms/csv-extractor",
+#     partitioning="horizontal",
+#     vantage6_version="5.0.0",
+#     code_url="https://github.com/frankcorneliusmartin/v6-csv-extractor-py",
+#     functions=[
+#         {
+#             "name": "read_csv",
+#             "description": "",
+#             "type": "federated",
+#             "databases": [
+#                 {
+#                     "name": "Database 1"
+#                 }
+#             ],
+#             "arguments": []
+#         },
+#         {
+#             "name": "preprocess",
+#             "description": "",
+#             "type": "federated",
+#             "databases": [
+#                 {
+#                     "name": "Database 1"
+#                 }
+#             ],
+
+#         }
+#     ]
+
+# )
