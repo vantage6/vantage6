57e75d31a8bedb4d3829caa87f6fb1be2d808165
Bart van Beusekom <b.vanbeusekom@iknl.nl>
2025-03-03 12:21:34 +0100

    Black formatting

--- vantage6/vantage6/cli/__init__.py
+++ vantage6/vantage6/cli/__init__.py
@@ -1,3 +1,3 @@
-"""Command line interface for the vantage6 infrastructure. """
+"""Command line interface for the vantage6 infrastructure."""
 
 from ._version import version_info, __version__  # noqa: F401
