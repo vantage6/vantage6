2dfb281915116ee5215fd35268549e809bc7a0ab
F.C. Martin <f.martin@iknl.nl>
2025-03-13 15:27:14 +0100

    Update server attach command to use subprocess for log retrieval and remove deprecated options

--- vantage6/vantage6/cli/server/attach.py
+++ vantage6/vantage6/cli/server/attach.py
@@ -1,63 +1,16 @@
-import time
-from threading import Thread
-
 import click
-import questionary as q
-import docker
 
-from colorama import Fore, Style
-
-from vantage6.common import info, error
-from vantage6.common.docker.addons import check_docker_running
-from vantage6.common.globals import APPNAME, InstanceType
-
-from vantage6.cli.globals import DEFAULT_SERVER_SYSTEM_FOLDERS
-from vantage6.cli.common.utils import print_log_worker
+from subprocess import Popen, PIPE
+from vantage6.common import info
 
 
 @click.command()
-@click.option("-n", "--name", default=None, help="configuration name")
-@click.option("--system", "system_folders", flag_value=True)
-@click.option(
-    "--user", "system_folders", flag_value=False, default=DEFAULT_SERVER_SYSTEM_FOLDERS
-)
-def cli_server_attach(name: str, system_folders: bool) -> None:
+def cli_server_attach() -> None:
     """
     Show the server logs in the current console.
     """
-    check_docker_running()
-    client = docker.from_env()
+    info("Attaching to server logs...")
 
-    running_servers = client.containers.list(
-        filters={"label": f"{APPNAME}-type={InstanceType.SERVER}"}
-    )
-    running_server_names = [node.name for node in running_servers]
-
-    if not name:
-        try:
-            name = q.select(
-                "Select the server you wish to attach:", choices=running_server_names
-            ).unsafe_ask()
-        except KeyboardInterrupt:
-            error("Aborted by user!")
-            return
-    else:
-        post_fix = "system" if system_folders else "user"
-        name = f"{APPNAME}-{name}-{post_fix}-{InstanceType.SERVER}"
-
-    if name in running_server_names:
-        container = client.containers.get(name)
-        logs = container.attach(stream=True, logs=True, stdout=True)
-        Thread(target=print_log_worker, args=(logs,), daemon=True).start()
-        while True:
-            try:
-                time.sleep(1)
-            except KeyboardInterrupt:
-                info("Closing log file. Keyboard Interrupt.")
-                info(
-                    "Note that your server is still running! Shut it down "
-                    f"with {Fore.RED}v6 server stop{Style.RESET_ALL}"
-                )
-                exit(0)
-    else:
-        error(f"{Fore.RED}{name}{Style.RESET_ALL} was not running!?")
+    command = ["devspace", "logs", "--follow", "--label-selector", "app=vantage6-server"]
+    process = Popen(command, stdout=None, stderr=None)  # Redirecting output to the terminal
+    process.wait()  # Wait for the process to complete
