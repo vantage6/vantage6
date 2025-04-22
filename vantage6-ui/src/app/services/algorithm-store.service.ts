efde7aa70ecdfc7c9b744e34025b1f39cc3900bb
Djura Smits <djura.smits@gmail.com>
2025-04-22 14:13:41 +0200

    Reverted change to algorithm store server

--- vantage6-ui/src/app/services/algorithm-store.service.ts
+++ vantage6-ui/src/app/services/algorithm-store.service.ts
@@ -1,59 +1,53 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import {
-    AddAlgorithmStore,
-    AlgorithmStore,
-    AlgorithmStoreLazyProperties,
-    BaseAlgorithmStore,
-    EditAlgorithmStore,
-    StorePolicies
+  AddAlgorithmStore,
+  AlgorithmStore,
+  AlgorithmStoreLazyProperties,
+  BaseAlgorithmStore,
+  EditAlgorithmStore,
+  StorePolicies
 } from 'src/app/models/api/algorithmStore.model';
-import {ApiService} from './api.service';
-import {environment} from 'src/environments/environment';
-import {Pagination} from 'src/app/models/api/pagination.model';
-import {getLazyProperties} from 'src/app/helpers/api.helper';
+import { ApiService } from './api.service';
+import { environment } from 'src/environments/environment';
+import { Pagination } from 'src/app/models/api/pagination.model';
+import { getLazyProperties } from 'src/app/helpers/api.helper';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class AlgorithmStoreService {
-    constructor(private apiService: ApiService) {
+  constructor(private apiService: ApiService) {}
+
+  async getAlgorithmStores(): Promise<BaseAlgorithmStore[]> {
+    const result = await this.apiService.getForApi<Pagination<BaseAlgorithmStore>>(`/algorithmstore`, { per_page: 9999 });
+    return result.data;
+  }
+
+  async getAlgorithmStore(id: string, lazyProperties: AlgorithmStoreLazyProperties[] = []): Promise<AlgorithmStore> {
+    const result = await this.apiService.getForApi<AlgorithmStore>(`/algorithmstore/${id}`);
+
+    const store: AlgorithmStore = { ...result, collaborations: [], all_collaborations: result.collaborations === null };
+    if (!result.all_collaborations) {
+      await getLazyProperties(result, store, lazyProperties, this.apiService);
     }
 
-    async getAlgorithmStores(): Promise<BaseAlgorithmStore[]> {
-        const result = await this.apiService.getForApi<Pagination<BaseAlgorithmStore>>(`/algorithmstore`, {per_page: 9999});
-        return result.data;
-    }
+    return store;
+  }
 
-    async getAlgorithmStore(id: string, lazyProperties: AlgorithmStoreLazyProperties[] = []): Promise<AlgorithmStore> {
-        const result = await this.apiService.getForApi<AlgorithmStore>(`/algorithmstore/${id}`);
+  async addAlgorithmStore(addAlgorithmStore: AddAlgorithmStore): Promise<void> {
+    return await this.apiService.postForApi<void>(`/algorithmstore`, addAlgorithmStore);
+  }
 
-        const store: AlgorithmStore = {
-            ...result,
-            collaborations: [],
-            all_collaborations: result.collaborations === null
-        };
-        if (!result.all_collaborations) {
-            await getLazyProperties(result, store, lazyProperties, this.apiService);
-        }
+  async edit(id: string, editAlgorithmStore: EditAlgorithmStore): Promise<AlgorithmStore> {
+    return await this.apiService.patchForApi<AlgorithmStore>(`/algorithmstore/${id}`, editAlgorithmStore);
+  }
 
-        return store;
-    }
+  async delete(id: string): Promise<void> {
+    return await this.apiService.deleteForApi(`/algorithmstore/${id}`, { server_url: `${environment.server_url}${environment.api_path}` });
+  }
 
-    async addAlgorithmStore(addAlgorithmStore: AddAlgorithmStore): Promise<void> {
-        return await this.apiService.postForApi<void>(`/algorithmstore`, addAlgorithmStore);
-    }
-
-    async edit(id: string, editAlgorithmStore: EditAlgorithmStore): Promise<AlgorithmStore> {
-        return await this.apiService.patchForApi<AlgorithmStore>(`/algorithmstore/${id}`, editAlgorithmStore);
-    }
-
-    async delete(id: string): Promise<void> {
-        return await this.apiService.deleteForApi(`/algorithmstore/${id}`, {server_url: `${environment.server_url}${environment.api_path}`});
-    }
-
-    async getAlgorithmStorePolicies(store_url: string, public_: boolean = false): Promise<StorePolicies> {
-        const endpoint = public_ ? '/policy/public' : '/policy';
-        return await this.apiService.getForAlgorithmApi<StorePolicies>(store_url, endpoint);
-    }
-
+  async getAlgorithmStorePolicies(store_url: string, public_: boolean = false): Promise<StorePolicies> {
+    const endpoint = public_ ? '/policy/public' : '/policy';
+    return await this.apiService.getForAlgorithmApi<StorePolicies>(store_url, endpoint);
+  }
 }
