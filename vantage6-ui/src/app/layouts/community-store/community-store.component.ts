4bfeb4e6db04f4deb7d3ae875cb0c5250f169d88
Djura Smits <djura.smits@gmail.com>
2025-04-29 14:17:40 +0200

    Cleanup

--- vantage6-ui/src/app/layouts/community-store/community-store.component.ts
+++ vantage6-ui/src/app/layouts/community-store/community-store.component.ts
@@ -3,8 +3,6 @@ import {MatProgressSpinner} from "@angular/material/progress-spinner";
 import {MatToolbar} from "@angular/material/toolbar";
 import {routePaths} from 'src/app/routes';
 import {RouterLink, RouterOutlet} from "@angular/router";
-import {BreadcrumbsComponent} from "../../components/breadcrumbs/breadcrumbs.component";
-import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
 
 @Component({
     selector: 'app-community-store',
@@ -14,10 +12,7 @@ import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/mater
         MatToolbar,
         RouterOutlet,
         RouterLink,
-        BreadcrumbsComponent,
-        MatSidenav,
-        MatSidenavContainer,
-        MatSidenavContent,
+
 
     ],
     styleUrl: './community-store.component.scss'
