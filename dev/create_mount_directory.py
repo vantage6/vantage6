6e906316fe233ca10204c16b103f877017175ce5
bartvanb <b.vanbeusekom@iknl.nl>
2025-05-20 13:14:13 +0200

    No longer redefine built-in python keyword

--- dev/create_mount_directory.py
+++ dev/create_mount_directory.py
@@ -1,5 +1,3 @@
-# echo "Creating tasks folder"
-# mkdir -p ${TASK_DIRECTORY}
 """
 Development script to create a directory on the host that will be mounted
 into one of the development containers.
@@ -10,7 +8,7 @@ import argparse
 from pathlib import Path
 
 
-def create_mount_directory(dir: Path):
+def create_mount_directory(dir_path: Path):
     """
     Create a directory on the host that will be mounted into one of the
     development containers.
@@ -19,17 +17,17 @@ def create_mount_directory(dir: Path):
     # If the directory contains /run/desktop/mnt/host/wsl, this will be replaced
     # by /mnt/wsl: this is an idiosyncrasy of WSL (for more details, see
     # https://dev.to/nsieg/use-k8s-hostpath-volumes-in-docker-desktop-on-wsl2-4dcl)
-    if str(dir).startswith("/run/desktop/mnt/host/wsl"):
-        dir = Path("/mnt/wsl") / dir.relative_to("/run/desktop/mnt/host/wsl")
+    if str(dir_path).startswith("/run/desktop/mnt/host/wsl"):
+        dir_path = Path("/mnt/wsl") / dir_path.relative_to("/run/desktop/mnt/host/wsl")
 
-    dir.mkdir(parents=True, exist_ok=True)
+    dir_path.mkdir(parents=True, exist_ok=True)
 
 
 def main():
     parser = argparse.ArgumentParser(description="Create a mount directory")
-    parser.add_argument("dir", type=Path, help="The directory to create")
+    parser.add_argument("dir_path", type=Path, help="The directory to create")
     args = parser.parse_args()
-    create_mount_directory(args.dir)
+    create_mount_directory(args.dir_path)
 
 
 if __name__ == "__main__":
