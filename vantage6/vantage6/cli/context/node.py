cb6c516ffe5c213e97b72a98393d2c2637f48845
Frank <mail@franky.codes>
2025-05-28 14:15:48 +0200

    Removed unneeded SSH and Squid volume name from the `NodeContext`

--- vantage6/vantage6/cli/context/node.py
+++ vantage6/vantage6/cli/context/node.py
@@ -189,34 +189,6 @@ class NodeContext(AppContext):
         return os.environ.get("DATA_VOLUME_NAME", f"{self.docker_container_name}-vol")
 
     @property
-    def docker_ssh_volume_name(self) -> str:
-        """
-        Docker volume in which the SSH configuration is stored.
-
-        Returns
-        -------
-        str
-            Docker volume name
-        """
-        return os.environ.get(
-            "SSH_TUNNEL_VOLUME_NAME", f"{self.docker_container_name}-ssh-vol"
-        )
-
-    @property
-    def docker_squid_volume_name(self) -> str:
-        """
-        Docker volume in which the SSH configuration is stored.
-
-        Returns
-        -------
-        str
-            Docker volume name
-        """
-        return os.environ.get(
-            "SSH_SQUID_VOLUME_NAME", f"{self.docker_container_name}-squid-vol"
-        )
-
-    @property
     def proxy_log_file(self):
         return self.log_file_name(type_="proxy_server")
 
