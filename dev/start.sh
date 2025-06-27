d32b0f3a5707e155f03225befa7d341fef9414bb
bartvanb <b.vanbeusekom@iknl.nl>
2025-06-27 14:07:52 +0200

    Fix original pipeline to spin up vantage6-auth and fix dev pipeline

--- dev/start.sh
+++ dev/start.sh
@@ -9,6 +9,11 @@
 
 POPULATE_MARKER=$1
 
+# Get the profile from environment variable if set
+PROFILE_FLAG=""
+if [ ! -z "${DEVSPACE_PROFILE}" ]; then
+  PROFILE_FLAG="-p ${DEVSPACE_PROFILE}"
+fi
 
 # Validate that the POPULATE_MARKER argument is provided
 if [ -z "${POPULATE_MARKER}" ]; then
@@ -24,17 +29,17 @@ if [ ! -f "${POPULATE_MARKER}" ]; then
   if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
     echo "Populating vantage6 server and starting development environment..."
     touch "${POPULATE_MARKER}"
-    devspace run-pipeline all-with-populate
+    devspace run-pipeline all-with-populate ${PROFILE_FLAG}
   else
     echo "Skipping populating vantage6 server."
     # Create the marker file to prevent asking the user again
     touch "${POPULATE_MARKER}"
-    devspace run-pipeline all-without-populate
+    devspace run-pipeline all-without-populate ${PROFILE_FLAG}
   fi
 else
   # Skip population if the marker file already exists
   echo "Skipping populating vantage6 server. The server is already populated: found marker '${POPULATE_MARKER}'."
-  devspace run-pipeline all-without-populate
+  devspace run-pipeline all-without-populate ${PROFILE_FLAG}
 fi
 
 
