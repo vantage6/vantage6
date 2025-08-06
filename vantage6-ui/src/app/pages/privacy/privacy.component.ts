f8e8b6a7f20d32bfb0fbbdc54f659d52b4e2e7c6
bartvanb <bartvanbeusekom@gmail.com>
2025-08-06 13:11:43 +0200

    Create buttons for contact links in privacy statement

--- vantage6-ui/src/app/pages/privacy/privacy.component.ts
+++ vantage6-ui/src/app/pages/privacy/privacy.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-privacy',
   templateUrl: './privacy.component.html',
   styleUrls: ['./privacy.component.scss'],
-  imports: [MatCard, MatCardContent, MatCardHeader, MatCardTitle, TranslateModule]
+  imports: [MatCard, MatCardContent, MatCardHeader, MatCardTitle, MatIcon, TranslateModule]
 })
-export class PrivacyComponent {
-  // Component logic can be added here if needed
-}
+export class PrivacyComponent {}
