69af4f0f78f2b7e73a6d3fa7de713a8dfa5b15d6
Frank <mail@franky.codes>
2025-09-11 10:24:25 +0200

    Added (untested) implementation of the new import function

--- vantage6/vantage6/cli/server/import_.py
+++ vantage6/vantage6/cli/server/import_.py
@@ -1,23 +1,18 @@
-import os
-from threading import Thread
-
 import click
-import docker
-from sqlalchemy.engine.url import make_url
+import requests
 
-from vantage6.common import info, warning
-from vantage6.common.docker.addons import check_docker_running, pull_image
+from vantage6.common import info, warning, error
 from vantage6.common.globals import (
     APPNAME,
-    DEFAULT_DOCKER_REGISTRY,
-    DEFAULT_SERVER_IMAGE,
     InstanceType,
 )
 
+from vantage6.cli import __version__
 from vantage6.cli.common.decorator import click_insert_context
-from vantage6.cli.common.utils import print_log_worker
 from vantage6.cli.context.server import ServerContext
 from vantage6.cli.utils import check_config_name_allowed
+
+from vantage6.client import UserClient
 
 
 # TODO this method has a lot of duplicated code from `start`
@@ -29,28 +24,8 @@ from vantage6.cli.utils import check_config_name_allowed
     default=False,
     help="Drop all existing data before importing",
 )
-@click.option("-i", "--image", default=None, help="Node Docker image to use")
-@click.option(
-    "--mount-src",
-    default="",
-    help="Override vantage6 source code in container with the source code in this path",
-)
-@click.option(
-    "--keep/--auto-remove",
-    default=False,
-    help="Keep image after finishing. Useful for debugging",
-)
-@click.option("--wait", default=False, help="Wait for the import to finish")
 @click_insert_context(type_=InstanceType.SERVER)
-def cli_server_import(
-    ctx: ServerContext,
-    file: str,
-    drop_all: bool,
-    image: str | None,
-    mount_src: str,
-    keep: bool,
-    wait: bool,
-) -> None:
+def cli_server_import(ctx: ServerContext, file: str, drop_all: bool) -> None:
     """
     Import vantage6 resources into a server instance.
 
@@ -60,90 +35,86 @@ def cli_server_import(
     The FILE_ argument should be a path to a yaml file containing the vantage6
     formatted data to import.
     """
-    # will print an error if not
-    check_docker_running()
+    info("Validating server version")
+    response = requests.get(f"{ctx.config['server']['url']}/api/version")
+    if response.status_code != 200:
+        error("Unable to get server version")
+        return
 
-    info("Starting server...")
-    info("Finding Docker daemon.")
-    docker_client = docker.from_env()
+    body = response.json()
+    if "version" not in body:
+        error("Server gave a valid response but did not include the version")
+        return
 
-    # check if name is allowed for docker volume, else exit
-    check_config_name_allowed(ctx.name)
-
-    # pull latest Docker image
-    if image is None:
-        image = ctx.config.get(
-            "image", f"{DEFAULT_DOCKER_REGISTRY}/{DEFAULT_SERVER_IMAGE}"
+    # Compare it to this package version
+    if server_version != __version__:
+        error(
+            f"You are using CLI version {__version__} but the server is running "
+            f"version {server_version}. Please use the same version of the CLI and "
+            "server."
         )
-    info(f"Pulling latest server image '{image}'.")
-    pull_image(docker_client, image)
+        return
 
-    info("Creating mounts")
-    mounts = [
-        docker.types.Mount("/mnt/config.yaml", str(ctx.config_file), type="bind"),
-        docker.types.Mount("/mnt/import.yaml", str(file), type="bind"),
-    ]
+    info("Validating import file")
+    with open(file, "r") as f:
+        import_data = yaml.safe_load(f)
+    # TODO: validate import file
 
-    # FIXME: code duplication with cli_server_start()
-    # try to mount database
-    uri = ctx.config["uri"]
-    url = make_url(uri)
-    environment_vars = None
-
-    if mount_src:
-        mount_src = os.path.abspath(mount_src)
-        mounts.append(docker.types.Mount("/vantage6", mount_src, type="bind"))
-
-    # If host is None, we're dealing with a file-based DB, like SQLite
-    if url.host is None:
-        db_path = url.database
-
-        if not os.path.isabs(db_path):
-            # We're dealing with a relative path here -> make it absolute
-            db_path = ctx.data_dir / url.database
-
-        basename = os.path.basename(db_path)
-        dirname = os.path.dirname(db_path)
-        os.makedirs(dirname, exist_ok=True)
-
-        # we're mounting the entire folder that contains the database
-        mounts.append(docker.types.Mount("/mnt/database/", dirname, type="bind"))
-
-        environment_vars = {
-            # ServerGlobals.DB_URI_ENV_VAR.value: f"sqlite:////mnt/database/{basename}"
-        }
-
-    else:
-        warning(
-            f"Database could not be transferred, make sure {url.host} "
-            "is reachable from the Docker container"
-        )
-        info("Consider using the docker-compose method to start a server")
-
-    drop_all_ = "--drop-all" if drop_all else ""
-    cmd = f"vserver-local import -c /mnt/config.yaml {drop_all_} /mnt/import.yaml"
-
-    info(cmd)
-
-    info("Run Docker container")
-    container = docker_client.containers.run(
-        image,
-        command=cmd,
-        mounts=mounts,
-        detach=True,
-        labels={
-            f"{APPNAME}-type": InstanceType.SERVER.value,
-            "name": ctx.config_file_name,
-        },
-        environment=environment_vars,
-        auto_remove=not keep,
-        tty=True,
+    # TODO: Call the vantage6 APIs (core and auth?) to import the data
+    client = UserClient(
+        server_url=ctx.config["server"]["url"],
+        uth_url=ctx.config["server"]["keycloakUrl"],
+        auth_realm=ctx.config["server"]["keycloakRealm"],
+        auth_client=ctx.config["server"]["keycloakClient"],
+        log_level="info",
     )
-    logs = container.logs(stream=True, stdout=True)
-    Thread(target=print_log_worker, args=(logs,), daemon=False).start()
 
-    info(f"Success! container id = {container.id}")
+    info("Authenticate using admin credentials")
+    client.authenticate()
 
-    if wait:
-        container.wait()
-        info("Container finished!")
+    info("Importing organizations")
+    organizations = []
+    for organization in import_data["organizations"]:
+        org = client.organization.create(
+            name=organization["name"],
+            address1=organization["address1"],
+            address2=organization["address2"],
+            zipcode=organization["zipcode"],
+            country=organization["country"],
+            domain=organization["domain"],
+            public_key=organization["public_key"],
+        )
+        organizations.append(org)
+
+        info("Collecting root role and rule")
+        root_role_id = client.role.list(name="Root")[0]["id"]
+
+        info(f"Importing users for organization {org['name']}")
+        for user in organization["users"]:
+            user = client.user.create(
+                username=user["username"],
+                firstname=user["firstname"],
+                lastname=user["lastname"],
+                password=user["password"],
+                organization=org["id"],
+                roles=[root_role_id],
+            )
+            users.append(user)
+
+    info("Importing collaborations")
+    for collaboration in import_data["collaborations"]:
+
+        # Collecting organization ids
+        organization_ids = []
+        for participant in collaboration["participants"]:
+            for org in organizations:
+                if org["name"] == participant["name"]:
+                    organization_ids.append(org["id"])
+
+        collaboration = client.collaboration.create(
+            name=collaboration["name"],
+            organization_ids=organization_ids,
+            encrypted=collaboration["encrypted"],
+        )
+
+    pass
