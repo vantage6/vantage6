d8ee191b16b9139d8eba874aa4c89aad98d58fcd
Frank <mail@franky.codes>
2025-10-01 13:49:02 +0200

    Redundant imports to explicitly define public interface for the module see ruff guidelines

--- vantage6-algorithm-tools/vantage6/algorithm/decorator/__init__.py
+++ vantage6-algorithm-tools/vantage6/algorithm/decorator/__init__.py
@@ -1,11 +1,17 @@
-# ruff: noqa: F401
 from vantage6.algorithm.decorator.action import (
-    central,
-    data_extraction,
-    federated,
-    preprocessing,
+    central as central,
+    data_extraction as data_extraction,
+    federated as federated,
+    preprocessing as preprocessing,
 )
-from vantage6.algorithm.decorator.algorithm_client import algorithm_client
-from vantage6.algorithm.decorator.data import dataframe, dataframes
-from vantage6.algorithm.decorator.metadata import metadata
-from vantage6.algorithm.decorator.ohdsi import omop_data_extraction
+from vantage6.algorithm.decorator.algorithm_client import (
+    algorithm_client as algorithm_client,
+)
+from vantage6.algorithm.decorator.data import (
+    dataframe as dataframe,
+    dataframes as dataframes,
+)
+from vantage6.algorithm.decorator.metadata import metadata as metadata
+from vantage6.algorithm.decorator.ohdsi import (
+    omop_data_extraction as omop_data_extraction,
+)
