bbc9d967f269fb9cc4ef3fdd1ae740f21e4403f2
Frank <mail@franky.codes>
2025-10-06 16:32:02 +0200

    Changed redundant imports for the `__all__` decorator

--- vantage6-algorithm-tools/vantage6/algorithm/decorator/__init__.py
+++ vantage6-algorithm-tools/vantage6/algorithm/decorator/__init__.py
@@ -1,17 +1,27 @@
 from vantage6.algorithm.decorator.action import (
-    central as central,
-    data_extraction as data_extraction,
-    federated as federated,
-    preprocessing as preprocessing,
+    central,
+    data_extraction,
+    federated,
+    preprocessing,
 )
 from vantage6.algorithm.decorator.algorithm_client import (
-    algorithm_client as algorithm_client,
+    algorithm_client,
 )
 from vantage6.algorithm.decorator.data import (
-    dataframe as dataframe,
-    dataframes as dataframes,
+    dataframe,
+    dataframes,
 )
-from vantage6.algorithm.decorator.metadata import metadata as metadata
-from vantage6.algorithm.decorator.ohdsi import (
-    omop_data_extraction as omop_data_extraction,
-)
+from vantage6.algorithm.decorator.metadata import metadata
+from vantage6.algorithm.decorator.ohdsi import omop_data_extraction
+
+__all__ = [
+    "central",
+    "data_extraction",
+    "federated",
+    "preprocessing",
+    "algorithm_client",
+    "dataframe",
+    "dataframes",
+    "metadata",
+    "omop_data_extraction",
+]
