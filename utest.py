ddba9a1907d76bf4667a26403e92dc415f4433f4
bartvanb <bartvanbeusekom@gmail.com>
2025-10-23 10:58:24 +0200

    Move CLI tests at the end of the unittests
    Running the CLI tests before the server tests had a weird effect on the latter causing two tests to fail. Due to time constraints, not looking further into this at present - we should move to non-unittest tests anyway

--- utest.py
+++ utest.py
@@ -51,12 +51,6 @@ def run():
         success_common = run_tests(common_test_suites)
         success = success and success_common
 
-    # run CLI tests
-    if args.cli or args.all:
-        cli_test_suites = find_tests(str(Path(__file__).parent / "vantage6"))
-        success_cli = run_tests(cli_test_suites)
-        success = success and success_cli
-
     # run algorithm store tests
     if args.algorithm_store or args.all:
         algorithm_store_test_suites = find_tests(
@@ -84,6 +78,12 @@ def run():
         node_test_suites = find_tests(str(Path(__file__).parent / "vantage6-node"))
         success_node = run_tests(node_test_suites)
         success = success and success_node
+
+    # run CLI tests
+    if args.cli or args.all:
+        cli_test_suites = find_tests(str(Path(__file__).parent / "vantage6"))
+        success_cli = run_tests(cli_test_suites)
+        success = success and success_cli
 
     sys.exit(not success)
 
